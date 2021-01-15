f1894a91a21ae40f6d3466e9f6efb4cfe6a1199f
Matt Weber <matt.weber@mts.com>
2021-01-15 12:12:26 -0600

    Finished ExtensibilityContainer, adding two new export types to hold indirect references to exports accompanied by metadata. Removed ExportExtensions as all functionality can be held within the ExtensibilityContainer.

---
+++ src/Odin/Extensibility/DisposableLazyExport.cs
@@ -0,0 +1,42 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2021 Bad Echo LLC. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+
+namespace BadEcho.Odin.Extensibility
+{
+    /// <summary>
+    /// Provides a disposable lazy indirect reference to an exported object and its associated metadata.
+    /// </summary>
+    /// <typeparam name="TContract">The contractual type of value being exported.</typeparam>
+    /// <typeparam name="TMetadata">The type of metadata accompanying the export.</typeparam>
+    internal sealed class DisposableLazyExport<TContract,TMetadata> : LazyExport<TContract,TMetadata>, IDisposable
+    {
+        private readonly IDisposable _disposable;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DisposableLazyExport{TContract,TMetadata}"/> class.
+        /// </summary>
+        /// <param name="valueFactory">The value factory that returns the exported value.</param>
+        /// <param name="metadata">The metadata value accompanying the export.</param>
+        /// <param name="disposable">
+        /// The disposable delay-exported object itself, to be disposed upon the disposal of this object.
+        /// </param>
+        public DisposableLazyExport(Func<TContract> valueFactory, IDictionary<string, object?> metadata, IDisposable disposable)
+            : base(valueFactory, metadata)
+        {
+            _disposable = disposable;
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            _disposable.Dispose();
+        }
+    }
+}
--- src/Odin/Extensibility/Hosting/ExtensibilityContainer.cs
+++ src/Odin/Extensibility/Hosting/ExtensibilityContainer.cs
@@ -5,10 +5,14 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
 using System.ComponentModel.Composition.Hosting;
 using System.ComponentModel.Composition.Primitives;
 using System.Linq;
+using BadEcho.Odin.Extensions;
+using BadEcho.Odin.Properties;
 
 namespace BadEcho.Odin.Extensibility.Hosting
 {
@@ -16,6 +20,10 @@ namespace BadEcho.Odin.Extensibility.Hosting
     /// Provides a container for <see cref="ComposablePart"/> objects as well the means for their composition
     /// within Odin's Extensibility framework.
     /// </summary>
+    /// <suppressions>
+    /// ReSharper disable SuspiciousTypeConversion.Global
+    /// False positives occur. There are types that derive from ExportDefinition and ICompositionElement.
+    /// </suppressions>
     internal sealed class ExtensibilityContainer : CompositionContainer
     {
         /// <summary>
@@ -35,14 +43,78 @@ namespace BadEcho.Odin.Extensibility.Hosting
         /// </summary>
         /// <typeparam name="T">The contractual type of export to process.</typeparam>
         /// <param name="definiens">Definiens used to constrain the extent of composition and ultimately the result of this method.</param>
-        /// <returns>A collection of values from all exports satisfying the constraints imposed by <c>definiens</c>.</returns>
+        /// <returns>
+        /// A collection of <typeparamref name="T"/> values from all exports satisfying the constraints imposed by <c>definiens</c>.
+        /// </returns>
         public IEnumerable<T> GetExportedValues<T>(ImportDefiniens definiens)
         {
             ImportDefinition definition = definiens.DefineImport();
             IEnumerable<Export> exports = GetExports(definition);
 
             // As MEF doesn't use deferred execution when returning exported values, we won't either.
-            return exports.Select(export => export.ToValue<T>()).ToList();
+            return exports.Select(ConvertToValue<T>).ToList();
+        }
+
+        /// <summary>
+        /// Composes (if necessary) and retrieves lazy values coupled with metadata from all the exports that satisfy the constraints
+        /// imposed by the provided definiens.
+        /// </summary>
+        /// <typeparam name="TContract">The contractual type of export to process.</typeparam>
+        /// <typeparam name="TMetadata">The type of exported metadata required.</typeparam>
+        /// <param name="definiens">Definiens used to constrain the extent of composition and ultimately the result of this method.</param>
+        /// <returns>
+        /// A collection of <see cref="Lazy{TContract,TMetadata}"/> values from all exports satisfying the constraints imposed by
+        /// <c>definiens</c>.
+        /// </returns>
+        public IEnumerable<Lazy<TContract,TMetadata>> GetExports<TContract,TMetadata>(ImportDefiniens definiens)
+        {
+            ImportDefinition definition = definiens.DefineImport();
+            IEnumerable<Export> exports = GetExports(definition, null);
+
+            // As MEF doesn't use deferred execution when returning exported values, we won't either.
+            return exports.Select(ConvertToValue<TContract, TMetadata>).ToList();
+        }
+
+        private static ICompositionElement ConvertToElement(Export export)
+        {
+            if (export is ICompositionElement element)
+                return element;
+
+            return export.Definition is ICompositionElement definitionElement
+                ? definitionElement
+                : new CompositionElement(export.Definition);
+        }
+
+        private static T ConvertToValue<T>(Export export)
+        {
+            object? exportedValue = export.Value;
+
+            if (exportedValue == null)
+                throw new ArgumentException(Strings.ArgumentExportValueNoObject, nameof(export));
+
+            if (exportedValue is ExportedDelegate exportedDelegate)
+                exportedValue = exportedDelegate.CreateDelegate(typeof(T).UnderlyingSystemType);
+
+            if (exportedValue is not T castValue)
+            {
+                throw new CompositionContractMismatchException(
+                    Strings.CompositionInvalidExportType.InvariantFormat(ConvertToElement(export).DisplayName,
+                                                                         typeof(T)));
+            }
+
+            return castValue;
+        }
+
+        private static Lazy<TContract, TMetadata> ConvertToValue<TContract, TMetadata>(Export export)
+        {
+            if (export is IDisposable disposableExport)
+            {
+                return new DisposableLazyExport<TContract, TMetadata>(() => ConvertToValue<TContract>(export),
+                                                                      export.Metadata,
+                                                                      disposableExport);
+            }
+
+            return new LazyExport<TContract, TMetadata>(() => ConvertToValue<TContract>(export), export.Metadata);
         }
     }
 }
