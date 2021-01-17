ee12808e27530ae7c948d21c63db04a114e54d0e
Matt Weber <matt.weber@mts.com>
2021-01-17 07:46:05 -0600

    Reordered existing SuppressMessageAttribute parameters so that the Justification is the last parameter specified (since we spend the most time, a lot of times at least, figuring out the Target value).
    Suppressed erroneous CA1812 on the CompositionElement debugger proxy type.

--- src/Fenestra/Properties/AssemblyInfo.cs
+++ src/Fenestra/Properties/AssemblyInfo.cs
@@ -10,12 +10,12 @@ using System.Windows.Markup;
 
 [assembly: SuppressMessage("Microsoft.Maintainability",
                            "CA1501",
-                           Justification = "Base classes are Microsoft controlled, cannot influence their hierarchy.",
                            Scope = "type",
-                           Target = "BadEcho.Fenestra.Controls.OutlinedTextElement")]
+                           Target = "BadEcho.Fenestra.Controls.OutlinedTextElement",
+                           Justification = "Base classes are Microsoft controlled, cannot influence their hierarchy.")]
 
 [assembly:SuppressMessage("Microsoft.Design",
                           "CA1045",
-                          Justification = "The method is protected, not fully public, and the immeasurable amount of convenience provided by this function vastly outweighs the inconvenience of passing an argument by reference. On top of all that, there is simply no way to provide automatic property notification without requiring the backing field to be passed by reference.",
                           Scope = "member",
-                          Target = "~M:BadEcho.Fenestra.ViewModels.ViewModel.NotifyIfChanged``1(``0@,``0,System.String)~System.Boolean")]
+                          Target = "~M:BadEcho.Fenestra.ViewModels.ViewModel.NotifyIfChanged``1(``0@,``0,System.String)~System.Boolean",
+                          Justification = "The method is protected, not fully public, and the immeasurable amount of convenience provided by this function vastly outweighs the inconvenience of passing an argument by reference. On top of all that, there is simply no way to provide automatic property notification without requiring the backing field to be passed by reference.")]
--- src/Odin/Properties/AssemblyInfo.cs
+++ src/Odin/Properties/AssemblyInfo.cs
@@ -19,6 +19,12 @@ using System.Runtime.InteropServices;
 
 [assembly: SuppressMessage("Microsoft.Design",
                            "CA1045",
-                           Justification = "System.Text.Json.Serialization.JsonConverter design is centered around the use of the System.Text.Json.Utf8JsonReader being passed around by reference.",
                            Scope = "member",
-                           Target = "BadEcho.Odin.Serialization.JsonPolymorphicConverter`2.ReadFromDescriptor")]
+                           Target = "BadEcho.Odin.Serialization.JsonPolymorphicConverter`2.ReadFromDescriptor",
+                           Justification = "System.Text.Json.Serialization.JsonConverter design is centered around the use of the System.Text.Json.Utf8JsonReader being passed around by reference.")]
+
+[assembly: SuppressMessage("Microsoft.Performance",
+                           "CA1812",
+                           Scope = "type",
+                           Target = "BadEcho.Odin.Extensibility.CompositionElement+CompositionElementDebuggerProxy",
+                           Justification = "Not entirely sure why this is being flagged. Besides being actually set up as a display proxy for CompositionElement, the Roslyn analyzers, according to the GitHub at least, are supposed to have support for types specified as parameters decorated with DynamicallyAccessedMembersAttribute, as is the case with DebuggerTypeProxyAttribute's Type parameter. This is either a bug, or the analyzers don't actually support DynamicallyAccessedMembersAttribute usage for this rule.")]
