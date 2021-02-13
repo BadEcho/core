71efa4bd19092110f53e124767c609002ab519e9
Matt Weber <matt.weber@mts.com>
2021-02-13 05:54:37 -0600

    Added host adapter interface.

---
+++ src/Odin/Extensibility/Hosting/IHostAdapter.cs
@@ -0,0 +1,25 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2021 Bad Echo LLC. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace BadEcho.Odin.Extensibility.Hosting
+{
+    /// <summary>
+    /// Defines an adapter that is responsible for establishing connections between a host and routable
+    /// plugin adapters that segment a common contract.
+    /// </summary>
+    public interface IHostAdapter
+    {
+        /// <summary>
+        /// Routes this call to the appropriate call-routable plugin contract implementation.
+        /// </summary>
+        /// <param name="methodName">The name of the contract method to execute.</param>
+        /// <returns>
+        /// The particular contract implementation that has been registered to execute the method specified by <c>methodName</c>.
+        /// </returns>
+        object Route(string methodName);
+    }
+}
