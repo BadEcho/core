8ee146b92a5a456a6aa75aae7bb8961e51f1e27f
Matt Weber <matt.weber@mts.com>
2021-02-28 16:15:02 -0600

    Added a generic variant for the base EventArgs class.

---
+++ src/Odin/EventArgs.cs
@@ -0,0 +1,31 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2021 Bad Echo LLC. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+
+namespace BadEcho.Odin
+{
+    /// <summary>
+    /// Provides a base generic class for classes that contain event data.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class EventArgs<T> : EventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EventArgs{T}"/> class.
+        /// </summary>
+        /// <param name="data">The event data.</param>
+        public EventArgs(T data) 
+            => Data = data;
+
+        /// <summary>
+        /// Gets the event data.
+        /// </summary>
+        public T Data
+        { get; }
+    }
+}
