3fe31c131f2fc77ba3d23fca1659bc16ec854dc5
Matt Weber <matt.weber@mts.com>
2021-04-27 02:07:19 -0500

    Trampoline Operations
    Added support for tail-recursive styled operations that mimic tail call
    optimizations.

---
+++ src/Odin/Bounce.cs
@@ -0,0 +1,41 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2021 Bad Echo LLC. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace BadEcho.Odin
+{
+    /// <summary>
+    /// Provides a bouncing method used in a trampolined recursive operation.
+    /// </summary>
+    public sealed class Bounce
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Bounce"/> class.
+        /// </summary>
+        private Bounce()
+        { }
+
+        /// <summary>
+        /// Gets a value indicating if the method is done bouncing, marking the end of the trampolined operation.
+        /// </summary>
+        public bool IsFinished
+        { get; private init; }
+
+        /// <summary>
+        /// Bounces the next method in the call chain.
+        /// </summary>
+        /// <returns>The <see cref="Bounce"/> for the next method in the call chain.</returns>
+        public static Bounce Continue()
+            => new();
+
+        /// <summary>
+        /// Bounces the final method in the call chain, marking the end of hte trampolined operation.
+        /// </summary>
+        /// <returns>The final <see cref="Bounce"/> in the call chain.</returns>
+        public static Bounce Finish()
+            => new() {IsFinished = true};
+    }
+}
---
+++ src/Odin/Trampoline.cs
@@ -0,0 +1,35 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2021 Bad Echo LLC. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+
+namespace BadEcho.Odin
+{
+    /// <summary>
+    /// Provides a trampoline operation, which allows for a tail-recursive styled operation to be executed as if it was
+    /// implemented in a language supporting tail call optimizations.
+    /// </summary>
+    public sealed class Trampoline
+    {
+        /// <summary>
+        /// Executes a method that takes no arguments in a recursive fashion until the end of the trampolined call chain has been
+        /// reached.
+        /// </summary>
+        /// <param name="method">The method to execute.</param>
+        public static void Execute(Func<Bounce> method)
+        {
+            Require.NotNull(method, nameof(method));
+
+            Bounce bouncingMethod = Bounce.Continue();
+
+            while (!bouncingMethod.IsFinished)
+            {
+                bouncingMethod = method();
+            }
+        }
+    }
+}
