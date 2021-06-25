f0a0b8480917213351f6af20d8643a222c08cd08
Matt Weber <matt@badecho.com>
2021-06-24 23:01:41 -0500

    Created concept of a Vision pluggable module.
    Corrected ResourceDictionary source not pointing to the new
    Vision.Extensibilty.dll.

---
+++ src/Vision.Extensibility/IVisionModule.cs
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2021 Bad Echo LLC. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using BadEcho.Odin.Extensibility;
+
+namespace BadEcho.Omnified.Vision.Extensibility
+{
+    /// <summary>
+    /// Defines a snap-in module granting vision to Omnified data.
+    /// </summary>
+    public interface IVisionModule : IFilterableFamily
+    {
+        /// <summary>
+        /// Gets the direction the module will grow from its anchor point.
+        /// </summary>
+        ModuleGrowthDirection GrowthDirection { get; }
+    }
+}
---
+++ src/Vision.Extensibility/ModuleGrowthDirection.cs
@@ -0,0 +1,28 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2021 Bad Echo LLC. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace BadEcho.Omnified.Vision.Extensibility
+{
+    /// <summary>
+    /// Specifies the direction a Vision module grows from its anchor point.
+    /// </summary>
+    public enum ModuleGrowthDirection
+    {
+        /// <summary>
+        /// The module does  not grow.
+        /// </summary>
+        None,
+        /// <summary>
+        /// The module will grow opposite of its anchor point on the horizontal plane.
+        /// </summary>
+        Horizontal,
+        /// <summary>
+        /// The module will grow opposite of its anchor point on the vertical plane.
+        /// </summary>
+        Vertical
+    }
+}
