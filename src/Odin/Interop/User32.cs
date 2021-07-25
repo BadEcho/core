e1c3118f81dd3a9156a8891a8a2697df0d0efcc1
Matt Weber <matt@badecho.com>
2021-07-24 19:10:49 -0500

    Added interop for User32 Get/SetWindowLong operations.

---
+++ src/Odin/Interop/User32.cs
@@ -0,0 +1,87 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2021 Bad Echo LLC. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace BadEcho.Odin.Interop
+{
+    /// <summary>
+    /// Provides interoperability with the core user interface functionality of Windows.
+    /// </summary>
+    internal static class User32
+    {
+        private const string LIBRARY_NAME = "user32";
+
+        /// <summary>
+        /// Retrieves information about a specified window.
+        /// </summary>
+        /// <param name="hWnd">A handle to the window and, indirectly, the class to which the window belongs.</param>
+        /// <param name="attribute">An enumeration value that specifies the window attribute to retrieve.</param>
+        /// <returns>The requested value if successful; otherwise, zero.</returns>
+        public static IntPtr GetWindowLongPtr(IntPtr hWnd, WindowAttribute attribute)
+            => IntPtr.Size == 4 ? GetWindowLongPtr32(hWnd, (int) attribute) : GetWindowLongPtr64(hWnd, (int) attribute);
+
+        /// <summary>
+        /// Changes an attribute for the specified window.
+        /// </summary>
+        /// <param name="hWnd">A handle to the window and, indirectly, the class to which the window belongs.</param>
+        /// <param name="attribute">An enumeration value that specifies the window attribute to replace.</param>
+        /// <param name="dwNewLong">The replacement value.</param>
+        /// <returns></returns>
+        public static IntPtr SetWindowLongPtr(IntPtr hWnd, WindowAttribute attribute, IntPtr dwNewLong)
+            => IntPtr.Size == 4
+                ? SetWindowLongPtr32(hWnd, (int) attribute, dwNewLong)
+                : SetWindowLongPtr64(hWnd, (int) attribute, dwNewLong);
+
+        /// <summary>
+        /// Retrieves information about the specified window.
+        /// </summary>
+        /// <param name="hWnd">A handle to the window and, indirectly, the class to which the window belongs.</param>
+        /// <param name="nIndex">The zero-based offset to the value to be retrieved.</param>
+        /// <returns>The requested value if successful; otherwise, zero.</returns>
+        /// <remarks>This should only ever be called from a 32-bit machine.</remarks>
+        [DllImport(LIBRARY_NAME, CharSet = CharSet.Auto, EntryPoint = "GetWindowLong")]
+        [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+        private static extern IntPtr GetWindowLongPtr32(IntPtr hWnd, int nIndex);
+
+        /// <summary>
+        /// Retrieves information about the specified window.
+        /// </summary>
+        /// <param name="hWnd">A handle to the window and, indirectly, the class to which the window belongs.</param>
+        /// <param name="nIndex">The zero-based offset to the value to be retrieved.</param>
+        /// <returns>The requested value if successful; otherwise, zero.</returns>
+        /// <remarks>This should only ever be called from a 64-bit machine.</remarks>
+        [DllImport(LIBRARY_NAME, CharSet = CharSet.Auto, EntryPoint = "GetWindowLongPtr")]
+        [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+        private static extern IntPtr GetWindowLongPtr64(IntPtr hWnd, int nIndex);
+
+        /// <summary>
+        /// Changes an attribute of the specified window.
+        /// </summary>
+        /// <param name="hWnd">A handle to the window and, indirectly, the class to which the window belongs.</param>
+        /// <param name="nIndex">The zero-based offset to the value to be set.</param>
+        /// <param name="dwNewLong">The replacement value.</param>
+        /// <returns>The previous value of the specified offset if successful; otherwise, zero.</returns>
+        /// <remarks>This should only ever be called from a 32-bit machine.</remarks>
+        [DllImport(LIBRARY_NAME, CharSet = CharSet.Auto, EntryPoint = "SetWindowLong")]
+        [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+        private static extern IntPtr SetWindowLongPtr32(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+        /// <summary>
+        /// Changes an attribute of the specified window.
+        /// </summary>
+        /// <param name="hWnd">A handle to the window and, indirectly, the class to which the window belongs.</param>
+        /// <param name="nIndex">The zero-based offset to the value to be set.</param>
+        /// <param name="dwNewLong">The replacement value.</param>
+        /// <returns>The previous value of the specified offset if successful; otherwise, zero.</returns>
+        /// <remarks>This should only ever be called from a 64-bit machine.</remarks>
+        [DllImport(LIBRARY_NAME, CharSet = CharSet.Auto, EntryPoint = "SetWindowLongPtr")]
+        [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+        private static extern IntPtr SetWindowLongPtr64(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+    }
+}
---
+++ src/Odin/Interop/WindowAttribute.cs
@@ -0,0 +1,48 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2021 Bad Echo LLC. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+
+namespace BadEcho.Odin.Interop
+{
+    /// <summary>
+    /// Specifies an attribute of a window that can be retrieved with <see cref="User32.GetWindowLongPtr(IntPtr, WindowAttribute)"/>
+    /// and modified with <see cref="User32.SetWindowLongPtr(IntPtr, WindowAttribute, IntPtr)"/>.
+    /// </summary>
+    internal enum WindowAttribute
+    {
+        /// <summary>
+        /// The extended window style.
+        /// </summary>
+        ExtendedStyle = -20,
+        /// <summary>
+        /// A handle to the application instance.
+        /// </summary>
+        Instance = -6,
+        /// <summary>
+        /// A handle to the parent window, if one exists.
+        /// This cannot be set through <see cref="User32.SetWindowLongPtr(IntPtr, WindowAttribute, IntPtr)"/>.
+        /// </summary>
+        Parent = -8,
+        /// <summary>
+        /// The identifier of the window, if it is a child window.
+        /// </summary>
+        Identifier = -12,
+        /// <summary>
+        /// The window style.
+        /// </summary>
+        Style = -16,
+        /// <summary>
+        /// The user data associated with the window.
+        /// </summary>
+        UserData = -21,
+        /// <summary>
+        /// The pointer to the window procedure or a handle representing the pointer to the window procedure.
+        /// </summary>
+        WindowProcedure = -4
+    }
+}
