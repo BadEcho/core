2b38e6a839b38ada7c865c476aba202506991788
Matt Weber <matt@badecho.com>
2021-08-06 21:23:16 -0500

    Added license file and license header info to source.

--- src/Odin/Extensibility/Hosting/PluginContextStrategyExtensions.cs
+++ src/Odin/Extensibility/Hosting/PluginContextStrategyExtensions.cs
@@ -2,6 +2,12 @@
 // <copyright>
 //      Created by Matt Weber <matt@badecho.com>
 //      Copyright @ 2021 Bad Echo LLC. All rights reserved.
+//
+//		Bad Echo Technologies are licensed under a
+//		Creative Commons Attribution-NonCommercial 4.0 International License.
+//
+//		See accompanying file LICENSE.md or a copy at:
+//		http://creativecommons.org/licenses/by-nc/4.0/
 // </copyright>
 //-----------------------------------------------------------------------
 
