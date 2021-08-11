7a3f18db9a5bc248a85aafc6311eb9ec1ab68446
Matt Weber <matt@badecho.com>
2021-08-11 09:11:51 -0500

    Added a basic hot-key-fueled hiding function to the Vision application.

--- src/Vision/Windows/VisionWindow.cs
+++ src/Vision/Windows/VisionWindow.cs
@@ -14,9 +14,11 @@
 using System;
 using System.Windows.Interop;
 using BadEcho.Fenestra.Windows;
+using BadEcho.Odin;
 using BadEcho.Odin.Interop;
 using BadEcho.Omnified.Vision.Properties;
 using BadEcho.Omnified.Vision.ViewModels;
+using ModifierKeys = BadEcho.Odin.Interop.ModifierKeys;
 
 namespace BadEcho.Omnified.Vision.Windows
 {
@@ -25,6 +27,8 @@ namespace BadEcho.Omnified.Vision.Windows
     /// </summary>
     internal sealed class VisionWindow : Window<VisionViewModel>
     {
+        private NativeWindow? _native;
+        
         /// <summary>
         /// Initializes a new instance of the <see cref="VisionWindow"/> class.
         /// </summary>
@@ -53,9 +57,34 @@ namespace BadEcho.Omnified.Vision.Windows
             base.OnSourceInitialized(e);
 
             IntPtr handle = new WindowInteropHelper(this).Handle;
-            var nativeWindow = new NativeWindow(handle);
 
-            nativeWindow.MakeOverlay();
+            _native = new NativeWindow(handle, new PresentationWindowWrapper(handle));
+
+            _native.HotKeyPressed += HandleHotKeyPressed;
+
+            _native.RegisterHotKey(0, ModifierKeys.Shift, VirtualKey.L);
+            _native.MakeOverlay();
+        }
+
+        /// <inheritdoc/>
+        protected override void OnClosed(EventArgs e)
+        {
+            _native?.UnregisterHotKey(0);
+
+            base.OnClosed(e);
+        }
+
+        private void ToggleVisibility()
+        {
+            Visibility = Visibility == System.Windows.Visibility.Visible
+                ? System.Windows.Visibility.Collapsed
+                : System.Windows.Visibility.Visible;
+        }
+
+        private void HandleHotKeyPressed(object? sender, EventArgs<int> e)
+        {
+            if (e.Data == 0)
+                ToggleVisibility();
         }
     }
 }
