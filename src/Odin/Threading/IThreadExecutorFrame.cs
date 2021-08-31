d8f8388be80003071b49456a8892d4957c1a5740
Matt Weber <matt@badecho.com>
2021-08-31 16:00:33 -0500

    Added foundation for thread executor framework.

---
+++ src/Odin/Threading/IThreadExecutorFrame.cs
@@ -0,0 +1,31 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2021 Bad Echo LLC. All rights reserved.
+//
+//		Bad Echo Technologies are licensed under a
+//		Creative Commons Attribution-NonCommercial 4.0 International License.
+//
+//		See accompanying file LICENSE.md or a copy at:
+//		http://creativecommons.org/licenses/by-nc/4.0/
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace BadEcho.Odin.Threading
+{
+    /// <summary>
+    /// Defines a representation of a thread executor frame.
+    /// </summary>
+    public interface IThreadExecutorFrame
+    {
+        /// <summary>
+        /// Gets or sets a value indicating if this frame will exit when all frames are requested to exit.
+        /// </summary>
+        bool ExitUponRequest { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating if this frame should continue processing.
+        /// </summary>
+        bool ShouldContinue { get; set; }
+    }
+}
---
+++ src/Odin/Threading/ThreadExecutorOperation.cs
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2021 Bad Echo LLC. All rights reserved.
+//
+//		Bad Echo Technologies are licensed under a
+//		Creative Commons Attribution-NonCommercial 4.0 International License.
+//
+//		See accompanying file LICENSE.md or a copy at:
+//		http://creativecommons.org/licenses/by-nc/4.0/
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace BadEcho.Odin.Threading
+{
+    /// <summary>
+    /// Provides a representation of a delegate that has been posted to an executor's queue.
+    /// </summary>
+    public class ThreadExecutorOperation
+    {
+    }
+}
