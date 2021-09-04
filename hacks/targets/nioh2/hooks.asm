fe172c83f64279b28c14232c319423bdaea9574b
Matt Weber <matt@badecho.com>
2021-09-04 09:22:37 -0500

    Added target for Omnified Nioh 2.

---
+++ hacks/targets/nioh2/hooks.asm
@@ -0,0 +1,11 @@
+//----------------------------------------------------------------------
+// Hooks for Omnified Nioh 2
+// Written By: Matt Weber (https://badecho.com) (https://twitch.tv/omni)
+// Copyright 2021 Bad Echo LLC
+//
+// Bad Echo Technologies are licensed under a
+// Creative Commons Attribution-NonCommercial 4.0 International License.
+//
+// See accompanying file LICENSE.md or a copy at:
+// http://creativecommons.org/licenses/by-nc/4.0/
+//----------------------------------------------------------------------
