411a9b1012f0a58675e17ed9b1f5e4de5bc17f20
Matt Weber <matt@badecho.com>
2021-09-04 10:20:57 -0500

    Hook for current/max player health.

--- hacks/targets/nioh2/hooks.asm
+++ hacks/targets/nioh2/hooks.asm
@@ -9,3 +9,40 @@
 // See accompanying file LICENSE.md or a copy at:
 // http://creativecommons.org/licenses/by-nc/4.0/
 //----------------------------------------------------------------------
+
+// Gets the player's health structure.
+// Polls player health exclusively. No filtering required.
+// [rcx+20]: Current HP.
+// [rcx+18]: Maximum HP.
+// UNIQUE AOB: 3B 48 6B 41 20 64
+define(omniPlayerHealthHook,"nioh2.exe"+C700E0)
+
+assert(omniPlayerHealthHook,48 6B 41 20 64)
+alloc(getPlayerHealth,$1000,omniPlayerHealthHook)
+alloc(playerHealth,8)
+
+registersymbol(playerHealth)
+registersymbol(omniPlayerHealthHook)
+
+getPlayerHealth:
+    mov [playerHealth],rcx
+getPlayerHealthOriginalCode:
+    imul rax,[rcx+20],64
+    jmp getPlayerHealthReturn
+
+omniPlayerHealthHook:
+    jmp getPlayerHealth
+getPlayerHealthReturn:
+
+
+[DISABLE]
+
+// Cleanup of omniPlayerHealthHook
+omniPlayerHealthHook:
+    db 48 6B 41 20 64
+
+unregistersymbol(omniPlayerHealthHook)
+unregistersymbol(playerHealth)
+
+dealloc(playerHealth)
+dealloc(getPlayerHealth)
