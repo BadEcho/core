41789d0884e3617adbf75c8e204de9ed497a4142
Matt Weber <matt@badecho.com>
2021-10-07 03:39:22 -0500

    Added more Apocalypse event types.

---
+++ src/Vision.Apocalypse/BonusDamageType.cs
@@ -0,0 +1,33 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2021 Bad Echo LLC. All rights reserved.
+//
+//		Bad Echo Technologies are licensed under a
+//		Creative Commons Attribution-NonCommercial 4.0 International License.
+//
+//		See accompanying file LICENSE.md or a copy at:
+//		http://creativecommons.org/licenses/by-nc/4.0/
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace BadEcho.Omnified.Vision.Apocalypse
+{
+    /// <summary>
+    /// Specifies the type of bonus damage being applied to an Enemy Apocalypse event.
+    /// </summary>
+    public enum BonusDamageType
+    {
+        /// <summary>
+        /// A bonus stemming from a successful critical hit roll which multiplies the damage being done
+        /// to an enemy by a randomly determined amount, typically falling within a range of non-ridiculous
+        /// multipliers (i.e., 2.0x-5.0x).
+        /// </summary>
+        CriticalHit,
+        /// <summary>
+        /// A bonus stemming from a successful, and quite rare, Kamehameha roll, typically applying a completely
+        /// ridiculous multiplier to the damage (i.e., 10000x).
+        /// </summary>
+        Kamehameha,
+    }
+}
---
+++ src/Vision.Apocalypse/ExtraDamageEvent.cs
@@ -0,0 +1,33 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2021 Bad Echo LLC. All rights reserved.
+//
+//		Bad Echo Technologies are licensed under a
+//		Creative Commons Attribution-NonCommercial 4.0 International License.
+//
+//		See accompanying file LICENSE.md or a copy at:
+//		http://creativecommons.org/licenses/by-nc/4.0/
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace BadEcho.Omnified.Vision.Apocalypse
+{
+    /// <summary>
+    /// Provides a description for the "extra damage" Player Apocalypse event that occurred in an Omnified game.
+    /// </summary>
+    /// <remarks>
+    /// This is, by default, the most common outcome from a Player Apocalypse event roll. For most games, the amount of
+    /// extra damage applied is 2.0, however this is increased if the game features particularly low amounts of damage.
+    /// </remarks>
+    public sealed class ExtraDamageEvent : PlayerApocalypseEvent
+    {
+        /// <summary>
+        /// Gets the multiplier applied to incoming damage amounts that are being subjected to an "extra damage"
+        /// random effect.
+        /// </summary>
+        public double ExtraDamageMultiplier
+        { get; init; }
+    }
+}
+ 
