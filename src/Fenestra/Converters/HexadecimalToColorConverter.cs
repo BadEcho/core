21fc66044c7c8836342d395210445fe4ae970b4e
Matt Weber <matt@badecho.com>
2021-10-12 21:18:37 -0500

    Added hex->color and percentage to Point converters.

---
+++ src/Fenestra/Converters/HexadecimalToColorConverter.cs
@@ -0,0 +1,52 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2021 Bad Echo LLC. All rights reserved.
+//
+//		Bad Echo Technologies are licensed under a
+//		Creative Commons Attribution-NonCommercial 4.0 International License.
+//
+//		See accompanying file LICENSE.md or a copy at:
+//		http://creativecommons.org/licenses/by-nc/4.0/
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace BadEcho.Fenestra.Converters
+{
+    /// <summary>
+    /// Provides a value converter that converts provided hexadecimal <see cref="string"/> values to
+    /// <see cref="Color"/> values.
+    /// </summary>
+    [ValueConversion(typeof(string), typeof(Color))]
+    public sealed class HexadecimalToColorConverter : ValueConverter<string,Color>
+    {
+        /// <summary>
+        /// Gets or sets the color returned for string values not properly expressing a valid ARGB color.
+        /// </summary>
+        public Color FallbackColor
+        { get; set; } = Colors.Transparent;
+
+        /// <inheritdoc/>
+        protected override Color Convert(string value, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                return (Color) ColorConverter.ConvertFromString(value);
+            }
+            catch (FormatException)
+            {   // If this kind of exception is thrown, then input is not a valid color identifier.
+                // Don't want to propagate errors in an IValueConverter, so we return the fallback color.
+                return FallbackColor;
+            }
+        }
+
+        /// <inheritdoc/>
+        protected override string ConvertBack(Color value, object parameter, CultureInfo culture) 
+            => $"{value.A:X2}{value.R:X2}{value.G:X2}{value.B:X2}";
+    }
+}
---
+++ src/Fenestra/Converters/PercentageToPointConverter.cs
@@ -0,0 +1,97 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2021 Bad Echo LLC. All rights reserved.
+//
+//		Bad Echo Technologies are licensed under a
+//		Creative Commons Attribution-NonCommercial 4.0 International License.
+//
+//		See accompanying file LICENSE.md or a copy at:
+//		http://creativecommons.org/licenses/by-nc/4.0/
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Globalization;
+using System.Windows;
+
+namespace BadEcho.Fenestra.Converters
+{
+    /// <summary>
+    /// Provides a value converter that converts provided percentage values to a corresponding
+    /// x- and y-coordinate pair in two-dimensional space.
+    /// </summary>
+    public sealed class PercentageToPointConverter : ReversibleValueConverter<double,Point>
+    {
+        /// <summary>
+        /// Gets or sets the relationship between incoming percentage values and the x-coordinates
+        /// they are to be converted into.
+        /// </summary>
+        /// <remarks>
+        /// A ratio of 1 to 1 (which the default value of 1.0 represents) would result in an incoming
+        /// percentage value of 50% (0.5) being converted to a point where the x-coordinate is set to 0.5.
+        /// </remarks>
+        public double PercentageToXRatio
+        { get; set; } = 1.0;
+
+        /// <summary>
+        /// Gets or sets the relationship between incoming percentage values and the y-coordinates they
+        /// are to be converted into.
+        /// </summary>
+        /// <remarks>
+        /// A ratio of 1 to 1 (which the default value of 1.0 represents) would result in an incoming
+        /// percentage value of 50% (0.5) being converted to a point where the y-coordinate is set to 0.5.
+        /// </remarks>
+        public double PercentageToYRatio
+        { get; set; } = 1.0;
+
+        /// <summary>
+        /// Gets or sets the point value cascade amount that occurs when incoming percentage values transition
+        /// from whole amounts to proper fractional values.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// The relationship between incoming percentage values and the converted point values essentially linear,
+        /// except when the input percentages begin to fall underneath whole amounts and become fractional, which
+        /// would be anything less than 100% (0.1). When this occurs, a cascade can be applied which will apply an
+        /// immediate reductionary adjustment equal to this property's value.
+        /// </para>
+        /// <para>
+        /// In the opposite case, where incoming percentage values cross from fractional to whole, a reverse cascade
+        /// is applied. The default amount this is set to is 0, which means, by default, no cascading occurs.
+        /// </para>
+        /// <para>
+        /// An input value is considered "fractional" if it is a proper fraction, improper fractions are excluded
+        /// from any type of cascading effect, as they are essentially "whole amounts and then some". Yes,
+        /// excellent use of proper nomenclature, I know.
+        /// </para>
+        /// </remarks>
+        public double FractionalCascadeAmount
+        { get; set; }
+
+        /// <inheritdoc/>
+        protected override Point Convert(double value, object parameter, CultureInfo culture)
+        {
+            if (value >= 1.0)
+                value += FractionalCascadeAmount;
+
+            double xCoordinate = value * PercentageToXRatio;
+            double yCoordinate = value * PercentageToYRatio;
+
+            return new Point(xCoordinate, yCoordinate);
+        }
+
+        /// <inheritdoc/>
+        protected override double ConvertBack(Point value, object parameter, CultureInfo culture)
+        {   
+            // Not going to bother too much with intense validation here, as we don't want to ever throw errors
+            // in an IValueConverter. We assume the point value was created by this converter, and convert the
+            // value back based on the looking at the first coordinate point we come across.
+            double percentage = value.X / PercentageToXRatio;
+
+            if (percentage >= 1.0 + FractionalCascadeAmount)
+                percentage -= FractionalCascadeAmount;
+
+            return percentage;
+        }
+    }
+}
