89d7e1c4e9d9c05a44d1a4325b880af1cbdfe0e6
Matt Weber <matt@badecho.com>
2021-12-19 05:46:15 -0600

    Added wrapped execution class for executors.

---
+++ src/Odin/Threading/ThreadExecutorWrapper.cs
@@ -0,0 +1,109 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2021 Bad Echo LLC. All rights reserved.
+//
+//		Bad Echo Technologies are licensed under a
+//		Creative Commons Attribution-NonCommercial 4.0 International License.
+//
+//		See accompanying file LICENSE.md or a copy at:
+//		http://creativecommons.org/licenses/by-nc/4.0/
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace BadEcho.Odin.Threading;
+
+/// <summary>
+/// Provides method execution routines designed specifically for thread executor style invocations, optionally offering exception
+/// filtering.
+/// </summary>
+internal sealed class ThreadExecutorWrapper
+{
+    private readonly object _source;
+
+    /// <summary>
+    /// Occurs when an exception has been thrown and is available for filtering by subscribers.
+    /// </summary>
+    public event EventHandler<ThreadExceptionEventArgs>? Filter;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ThreadExecutorWrapper"/> class.
+    /// </summary>
+    /// <param name="source">The object using this wrapper.</param>
+    public ThreadExecutorWrapper(object source)
+    {
+        _source = source;
+    }
+    
+    /// <summary>
+    /// Executes the provided delegate in the most efficient way possible, optionally filtering out any thrown
+    /// exceptions if requested.
+    /// </summary>
+    /// <param name="method">The delegate to execute.</param>
+    /// <param name="argument">An optional argument to pass to the delegate.</param>
+    /// <param name="filterExceptions">Value indicating if exceptions should be filtered out.</param>
+    /// <returns>The result of executing <c>method</c>.</returns>
+    public object? Execute(Delegate method, object? argument, bool filterExceptions)
+    {
+        Require.NotNull(method, nameof(method));
+        object? result = null;
+
+        try
+        {
+            result = Execute(method, argument);
+        }
+        catch (Exception ex)
+        {
+            if (!filterExceptions || !FilterException(ex))
+                throw;
+        }
+
+        return result;
+    }
+
+    private static object? Execute(Delegate method, object? argument)
+    {
+        Require.NotNull(method, nameof(method));
+
+        object? result = null;
+
+        switch (method)
+        {
+            case Action action:
+                action();
+                break;
+
+            case Func<object> function:
+                result = function();
+                break;
+
+            case ThreadExecutorOperationCallback function:
+                result = function(argument);
+                break;
+
+            case SendOrPostCallback callback:
+                callback(argument);
+                break;
+
+            default:
+                method.DynamicInvoke(argument);
+                break;
+        }
+
+        return result;
+    }
+
+    private bool FilterException(Exception exception)
+    {
+        EventHandler<ThreadExceptionEventArgs>? filter = Filter;
+
+        if (filter == null)
+            return false;
+
+        var args = new ThreadExceptionEventArgs(exception);
+
+        filter(_source, args);
+
+        return args.Handled;
+    }
+}
