6fbd77094ea2b6b8a4eac4595f323f2f02565dbb
Matt Weber <matt@badecho.com>
2022-01-18 13:34:49 -0600

    Grammar fixin' in documentation.

--- src/Fenestra/Selectors/ViewContextTemplateSelector.cs
+++ src/Fenestra/Selectors/ViewContextTemplateSelector.cs
@@ -18,22 +18,22 @@ using BadEcho.Odin.Extensions;
 namespace BadEcho.Fenestra.Selectors;
 
 /// <summary>
-/// Provides a data template selector that honors data templates for data contexts targeting particular views and defined an assembly's
-/// <c>ViewContexts.xaml</c> file.
+/// Provides a data template selector that honors data templates for data contexts targeting particular views, defined in an
+/// assembly's <c>ViewContexts.xaml</c> file.
 /// </summary>
 public class ViewContextTemplateSelector : DataTemplateSelector
 {
     /// <inheritdoc/>
     /// <remarks>
     /// <para>
-    /// We first try to locate a data template linked to the specific objects type, but if one does not exist, we
+    /// We first try to locate a data template linked to the specific object's type, but if one does not exist, we
     /// fallback to the first template found associated with the most derived of interfaces for the object type.
     /// This is to avoid including templates targeting interfaces that act as a base to other interfaces, which themselves
     /// may have distinct concrete implementations.
     /// </para>
     /// <para>
-    /// Interfaces are supported because there are times we may want to support view model types existing in some sort of
-    /// object hierarchy utilizing generic type parameters, something which is not supported by the majority of XAML parsers.
+    /// Interfaces are useful for the times where we may want to support view model types that exist in some sort of
+    /// object hierarchy that uses generic type parameters, something which is not supported by the majority of XAML parsers.
     /// </para>
     /// </remarks>
     public override DataTemplate? SelectTemplate(object? item, DependencyObject container)
