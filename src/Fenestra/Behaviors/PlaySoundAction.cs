7cc422281628f05881a464ec7164cf8da3c1080f
Matt Weber <matt@badecho.com>
2022-01-18 13:16:38 -0600

    Effect sound playback polishing.
    Uninterruptibility added to action.

--- src/Fenestra/Behaviors/PlaySoundAction.cs
+++ src/Fenestra/Behaviors/PlaySoundAction.cs
@@ -31,12 +31,33 @@ public sealed class PlaySoundAction : BehaviorAction<DependencyObject>
                                       typeof(byte[]),
                                       typeof(PlaySoundAction));
     /// <summary>
+    /// Identifies the <see cref="IsUninterruptible"/> dependency property.
+    /// </summary>
+    public static readonly DependencyProperty IsUninterruptibleProperty
+        = DependencyProperty.Register(nameof(IsUninterruptible),
+                                      typeof(bool),
+                                      typeof(PlaySoundAction));
+    /// <summary>
     /// Gets or sets the array of bytes that compose the sound to play when this action is executed.
     /// </summary>
     public IEnumerable<byte>? Sound
     {
         get => (IEnumerable<byte>?) GetValue(SoundProperty);
         set => SetValue(SoundProperty, value);
+    }
+
+    /// <summary>
+    /// Gets or sets a value that, when true, indicates that the sound played by this action must be played for its entire duration
+    /// before another <see cref="PlaySoundAction"/> is allowed to begin its audio playback.
+    /// </summary>
+    /// <remarks>
+    /// If this is not set to true, the execution of any subsequent <see cref="PlaySoundAction"/> will result in this action's sound being
+    /// immediately interrupted. Got to make room for the new, baby!
+    /// </remarks>
+    public bool IsUninterruptible
+    {
+        get => (bool) GetValue(IsUninterruptibleProperty);
+        set => SetValue(IsUninterruptibleProperty, value);
     }
 
     /// <inheritdoc/>
@@ -52,20 +73,30 @@ public sealed class PlaySoundAction : BehaviorAction<DependencyObject>
 
         if (sound == null || !sound.Any())
             return false;
+
+        // The task thread won't have access to this dependency property.
+        bool isUninterruptible = IsUninterruptible;
             
-        Task.Run(() => PlaySound(sound));
+        Task.Run(() => PlaySound(sound, isUninterruptible));
 
         return true;
     }
 
-    private static void PlaySound(IEnumerable<byte> sound)
+    private static void PlaySound(IEnumerable<byte> sound, bool isUninterruptible)
     {
         using (var stream = new MemoryStream(sound.ToArray()))
         {
             using (var soundPlayer = new SoundPlayer(stream))
-            {   // A synchronous playback call is required here so we don't end up disposing the sound player prior to the sound
-                // playback completing.
-                soundPlayer.PlaySync();
+            {   // We call Load() first so that the stream data doesn't end up being loaded on separate worker thread, which is exactly what happens
+                // if we just immediately call Play(). Once the stream data is loaded, the providing memory stream is safe to dispose.
+                // On the same note, it is safe to dispose the SoundPlayer immediately after Play() returns, as it will offload the actual playing
+                // of the already loaded stream data onto a worker thread, which is responsible for cleaning up itself.
+                soundPlayer.Load();
+                
+                if (isUninterruptible)
+                    soundPlayer.PlaySync();
+                else
+                    soundPlayer.Play();
             }
         }
     }
