81763489a264a4e3b2a5fe60aba5be8fbebafc9f
Matt Weber <matt@badecho.com>
2022-03-09 07:58:26 -0600

    Changing license to GNU APL 3.0.

--- src/Presentation/ViewModels/CollectionViewModelEngine.cs
+++ src/Presentation/ViewModels/CollectionViewModelEngine.cs
@@ -3,11 +3,11 @@
 //      Created by Matt Weber <matt@badecho.com>
 //      Copyright @ 2022 Bad Echo LLC. All rights reserved.
 //
-//		Bad Echo Technologies are licensed under a
-//		Creative Commons Attribution-NonCommercial 4.0 International License.
+//		Bad Echo Technologies are licensed under the
+//		GNU Affero General Public License v3.0.
 //
 //		See accompanying file LICENSE.md or a copy at:
-//		http://creativecommons.org/licenses/by-nc/4.0/
+//		https://www.gnu.org/licenses/agpl-3.0.html
 // </copyright>
 //-----------------------------------------------------------------------  
 
