f2a423a339fe2f4872a69c5a9881349b56019da7
Matt Weber <matt@badecho.com>
2022-03-15 18:27:33 -0500

    Version changes + build script.

---
+++ build.ps1
@@ -0,0 +1,34 @@
+# Builds the Bad Echo solution.
+
+param (
+	[string]$BuildIdentifier
+)
+
+function Execute([scriptblock]$command) {
+	& $command
+	if ($lastexitcode -ne 0) {
+		throw ("Build command errored with exit code: " + $lastexitcode)
+	}
+}
+
+$artifacts = ".\artifacts"
+
+if (Test-Path $artifacts) {
+	Remove-Item $artifacts -Force -Recurse
+}
+
+Execute { & dotnet clean -c Release }
+
+if ($BuildIdentifier) {
+	$properties = [Xml] (Get-Content .\Directory.build.props)
+	$prereleaseIdentifier = [string] $properties.Project.PropertyGroup.PrereleaseIdentifier
+	$prereleaseIdentifier = $prereleaseIdentifier.Trim()
+	$versionSuffix = "$prereleaseIdentifier.$BuildIdentifier"
+	
+	Execute { & dotnet build -c Release --version-suffix $versionSuffix }
+	Execute { & dotnet pack -c Release -o $artifacts --no-build --version-suffix $versionSuffix }
+}
+else {
+	Execute { & dotnet build -c Release }
+	Execute { & dotnet pack -c Release -o $artifacts --no-build }
+}
