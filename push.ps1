b975ae116623eb8618e384ce64571781f8e1fe05
Matt Weber <matt@badecho.com>
2022-03-16 05:46:22 -0500

    Testing new build/push after syntax fix...

--- push.ps1
+++ push.ps1
@@ -11,7 +11,7 @@ elseif ([string]::IsNullOrEmpty($Env:PKG_URL)) {
 }
 else {
 	Get-ChildItem $artifacts -Filter "*.nupkg" | ForEach-Object {
-		Write-Host "$(scriptName): Pushing $($_.Name) to repository."
+		Write-Host "$($scriptName): Pushing $($_.Name) to repository."
 		dotnet nuget push $_ --source $Env:PKG_URL --api-key $Env:PKG_API_KEY
 		if ($lastexitcode -ne 0) {
 			throw ("Push command errored with exit code: " + $lastexitcode)
