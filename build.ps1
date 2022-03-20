30b817cbb1818fb0fc2dbafd51fd42ad57a08e57
Matt Weber <matt@badecho.com>
2022-03-20 10:27:28 -0500

    Versioning using commit distance

--- build.ps1
+++ build.ps1
@@ -1,7 +1,8 @@
 # Builds the Bad Echo solution.
 
 param (
-	[string]$BuildIdentifier
+	[string]$CommitId,
+	[string]$VersionDistance
 )
 
 function Execute([scriptblock]$command) {
@@ -21,15 +22,18 @@ if (Test-Path $artifacts) {
 	Remove-Item $artifacts -Force -Recurse
 }
 
-$buildCommand =  { & dotnet build -c Release }
-$packCommand = { & dotnet pack -c Release -o $artifacts --no-build }
+$versionSettings = Get-Content version.json | ConvertFrom-Json
+$majorVersion = $versionSettings[0].majorVersion
+$minorVersion = $versionSettings[0].minorVersion
+$patchVersion = $versionSettings[0].patchVersion
 
-if($BuildIdentifier) {
-	$properties = [Xml] (Get-Content .\Directory.build.props)
-	$prereleaseIdentifier = [string] $properties.Project.PropertyGroup.PrereleaseIdentifier
-	$prereleaseIdentifier = $prereleaseIdentifier.Trim()
+$buildCommand =  { & dotnet build -c Release -p:MajorVersion=$majorVersion -p:MinorVersion=$minorVersion -p:PatchVersion=$patchVersion }
+$packCommand = { & dotnet pack -c Release -o $artifacts --no-build -p:MajorVersion=$majorVersion -p:MinorVersion=$minorVersion -p:PatchVersion=$patchVersion }
+
+if($CommitId -and $VersionDistance) {	
+	$prereleaseId = $versionSettings[0].prereleaseId
 	
-	$versionCommand = "--version-suffix $prereleaseIdentifier.$BuildIdentifier"
+	$versionCommand = "--version-suffix $prereleaseId.$VersionDistance+$CommitId"
 
 	$buildCommand = AppendCommand($buildCommand.ToString(), $versionCommand)
 	$packCommand = AppendCommand($packCommand.ToString(), $versionCommand)
