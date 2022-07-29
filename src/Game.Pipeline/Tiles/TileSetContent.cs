988c7d84b0352eff64b47897a61d3559d26a7296
Matt Weber <matt@badecho.com>
2022-07-28 21:13:06 -0500

    Added content item for tile sets.

---
+++ src/Game.Pipeline/Tiles/TileSetContent.cs
@@ -0,0 +1,28 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2022 Bad Echo LLC. All rights reserved.
+//
+//		Bad Echo Technologies are licensed under a
+//		GNU Affero General Public License v3.0.
+//
+//		See accompanying file LICENSE.md or a copy at:
+//		https://www.gnu.org/licenses/agpl-3.0.html
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace BadEcho.Game.Pipeline.Tiles;
+
+/// <summary>
+/// Provides the raw data for a tile set asset.
+/// </summary>
+public sealed class TileSetContent : ContentItem<TileSetAsset>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TileSetContent"/> class.
+    /// </summary>
+    /// <param name="asset">The configuration data for a tile set.</param>
+    public TileSetContent(TileSetAsset asset) 
+        : base(asset)
+    { }
+}
