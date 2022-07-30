8542fb80311ee9e33cf1577ffdd450f5ee5d3684
Matt Weber <matt@badecho.com>
2022-07-29 23:33:42 -0500

    Extensiosn for all content related contexts.

--- src/Game.Pipeline/ContentContextExtensions.cs
+++ src/Game.Pipeline/ContentContextExtensions.cs
@@ -16,16 +16,29 @@ using Microsoft.Xna.Framework.Content.Pipeline;
 namespace BadEcho.Game.Pipeline;
 
 /// <summary>
-/// Provides a set of static methods intended to aid in matters related to asset importation into the content pipeline.
+/// Provides a set of static methods intended to aid in matters related to an asset's importation and processing into the
+/// content pipeline.
 /// </summary>
-internal static class ContentImporterExtensions
+internal static class ContentContextExtensions
 {
     /// <summary>
-    /// Logs a message during the process of importing an asset to the content pipeline.
+    /// Logs a message during the importation of an asset into the content pipeline.
     /// </summary>
     /// <param name="context">The current game asset importer's context.</param>
     /// <param name="message">The message to log.</param>
     public static void Log(this ContentImporterContext context, string message)
+    {
+        Require.NotNull(context, nameof(context));
+
+        context.Logger.LogMessage(message);
+    }
+
+    /// <summary>
+    /// Logs a message during the processing of an asset into the content pipeline.
+    /// </summary>
+    /// <param name="context">The current game asset's processor's context.</param>
+    /// <param name="message">The message to log.</param>
+    public static void Log(this ContentProcessorContext context, string message)
     {
         Require.NotNull(context, nameof(context));
 
