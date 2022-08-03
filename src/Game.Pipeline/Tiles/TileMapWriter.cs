bc84b48bb80ebe283723311a09d87e13cad36240
Matt Weber <matt@badecho.com>
2022-08-03 10:49:31 -0500

    Added a content pipeline reader and writer for tile maps.

---
+++ src/Game.Pipeline/Tiles/TileMapWriter.cs
@@ -0,0 +1,127 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2022 Bad Echo LLC. All rights reserved.
+//
+//		Bad Echo Technologies are licensed under a
+//		GNU Affero General Public License v3.0.
+//
+//		See accompanying file LICENSE.md or a copy at:
+//		https://www.gnu.org/licenses/agpl-3.0.html
+// </copyright>
+//-----------------------------------------------------------------------
+
+using BadEcho.Game.Tiles;
+using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
+using Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler;
+
+namespace BadEcho.Game.Pipeline.Tiles;
+
+/// <summary>
+/// Provides a writer of raw tile map content into the content pipeline.
+/// </summary>
+[ContentTypeWriter]
+public sealed class TileMapWriter : ContentTypeWriter<TileMapContent>
+{
+    /// <inheritdoc />
+    public override string GetRuntimeReader(TargetPlatform targetPlatform)
+        => typeof(TileMapReader).AssemblyQualifiedName ?? string.Empty;
+
+    /// <inheritdoc />
+    protected override void Write(ContentWriter output, TileMapContent value)
+    {
+        Require.NotNull(output, nameof(output));
+        Require.NotNull(value, nameof(value));
+
+        TileMapAsset asset = value.Asset;
+
+        output.Write((int) asset.Orientation);
+        output.Write((int) asset.RenderOrder);
+        output.Write(asset.BackgroundColor);
+        output.Write(asset.Width);
+        output.Write(asset.Height);
+        output.Write(asset.TileWidth);
+        output.Write(asset.TileHeight);
+
+        WriteTileSets(output, value);
+        WriteLayers(output, value);
+    }
+
+    private static void WriteTileSets(ContentWriter output, TileMapContent value)
+    {
+        TileMapAsset asset = value.Asset;
+        
+        // Need to record how many tile sets in order to properly direct the reader.
+        output.Write(asset.TileSets.Count);
+
+        foreach (TileSetAsset tileSet in asset.TileSets)
+        {
+            output.Write(tileSet.FirstId);
+            // The next value written indicates if the tile set is externally defined.
+            if (!string.IsNullOrEmpty(tileSet.Source))
+            {
+                output.Write(true);
+                output.WriteExternalReference(value.GetReference<TileSetContent>(tileSet.Source));
+            }
+            else
+            {
+                output.Write(false);
+                TileSetWriter.Write(output, tileSet, value);
+            }
+        }
+    }
+
+    private static void WriteLayers(ContentWriter output, TileMapContent value)
+    {
+        TileMapAsset asset = value.Asset;
+
+        // Need to record how many layers in order to properly direct the reader.
+        output.Write(asset.Layers.Count);
+
+        foreach (LayerAsset layer in asset.Layers)
+        {
+            output.Write((int) layer.Type);
+            output.Write(layer.Name);
+            output.Write(layer.IsVisible);
+            output.Write(layer.Opacity);
+            output.Write(layer.OffsetX);
+            output.Write(layer.OffsetY);
+
+            switch (layer)
+            {
+                case ImageLayerAsset imageLayer:
+                    WriteImageLayer(output, imageLayer, value);
+                    break;
+
+                case TileLayerAsset tileLayer:
+                    WriteTileLayer(output, tileLayer);
+                    break;
+            }
+        }
+    }
+
+    private static void WriteImageLayer(ContentWriter output, ImageLayerAsset imageLayer, TileMapContent value)
+    {
+        ExternalReference<Texture2DContent> imageReference
+            = value.GetReference<Texture2DContent>(imageLayer.Image.Source);
+
+        output.WriteExternalReference(imageReference);
+    }
+
+    private static void WriteTileLayer(ContentWriter output, TileLayerAsset tileLayer)
+    {
+        output.Write(tileLayer.Width);
+        output.Write(tileLayer.Height);
+
+        // Need to record how many tiles in order to properly direct the reader.
+        output.Write(tileLayer.Tiles.Count);
+
+        foreach (Tile tile in tileLayer.Tiles)
+        {
+            output.Write(tile.IdWithFlags);
+            output.Write(tile.ColumnIndex);
+            output.Write(tile.RowIndex);
+        }
+    }
+}
---
+++ src/Game/Tiles/TileMapReader.cs
@@ -0,0 +1,129 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2022 Bad Echo LLC. All rights reserved.
+//
+//		Bad Echo Technologies are licensed under a
+//		GNU Affero General Public License v3.0.
+//
+//		See accompanying file LICENSE.md or a copy at:
+//		https://www.gnu.org/licenses/agpl-3.0.html
+// </copyright>
+//-----------------------------------------------------------------------
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BadEcho.Game.Tiles;
+
+/// <summary>
+/// Provides a reader of raw tile map content from the content pipeline.
+/// </summary>
+public sealed class TileMapReader : ContentTypeReader<TileMap>
+{
+    /// <inheritdoc />
+    protected override TileMap Read(ContentReader input, TileMap existingInstance)
+    {
+        Require.NotNull(input, nameof(input));
+
+        var orientation = (MapOrientation) input.ReadInt32();
+        var renderOrder = (TileRenderOrder) input.ReadInt32();
+        var backgroundColor = input.ReadColor();
+        var width = input.ReadInt32();
+        var height = input.ReadInt32();
+        var tileWidth = input.ReadInt32();
+        var tileHeight = input.ReadInt32();
+
+        var size = new Point(width, height);
+        var tileSize = new Point(tileWidth, tileHeight);
+        var map = new TileMap(input.GetGraphicsDevice(),
+                              input.AssetName,
+                              size,
+                              tileSize,
+                              orientation,
+                              renderOrder,
+                              backgroundColor);
+
+        ReadTileSets(input, map);
+        ReadLayers(input, map);
+
+        return map;
+    }
+
+    private static void ReadTileSets(ContentReader input, TileMap map)
+    {
+        var tileSetsToRead = input.ReadInt32();
+
+        while (tileSetsToRead > 0)
+        {
+            var firstId = input.ReadInt32();
+            var isExternal = input.ReadBoolean();
+            var tileSet = isExternal
+                ? input.ReadExternalReference<TileSet>()
+                : TileSetReader.Read(input);
+
+            map.AddTileSet(tileSet, firstId);
+
+            tileSetsToRead--;
+        }
+    }
+
+    private static void ReadLayers(ContentReader input, TileMap map)
+    {
+        var layersToRead = input.ReadInt32();
+
+        while (layersToRead > 0)
+        {
+            var layerType = (LayerType) input.ReadInt32();
+            var name = input.ReadString();
+            var isVisible = input.ReadBoolean();
+            var opacity = input.ReadSingle();
+            var offsetX = input.ReadSingle();
+            var offsetY = input.ReadSingle();
+
+            switch (layerType)
+            {
+                case LayerType.Image:
+                    var imageTexture = input.ReadExternalReference<Texture2D>();
+
+                    var imageLayer = new ImageLayer(name,
+                                                    isVisible,
+                                                    opacity,
+                                                    new Vector2(offsetX, offsetY),
+                                                    imageTexture,
+                                                    Vector2.Zero);
+                    map.AddLayer(imageLayer);
+                    break;
+
+                case LayerType.Tile:
+                    var width = input.ReadInt32();
+                    var height = input.ReadInt32();
+
+                    var tilesToRead = input.ReadInt32();
+
+                    var tileLayer = new TileLayer(name,
+                                                  isVisible,
+                                                  opacity,
+                                                  new Vector2(offsetX, offsetY),
+                                                  new Point(width, height));
+
+                    while (tilesToRead > 0)
+                    {
+                        var idWithFlags = input.ReadUInt32();
+                        var columnIndex = input.ReadInt32();
+                        var rowIndex = input.ReadInt32();
+
+                        tileLayer.LoadTile(idWithFlags, columnIndex, rowIndex);
+                        tilesToRead--;
+                    }
+
+                    map.AddLayer(tileLayer);
+                    break;
+            }
+
+
+            layersToRead--;
+        }
+    }
+}
