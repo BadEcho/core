5bfae7b3e46a6e5b7aacd115246b86123931ee57
Matt Weber <matt@badecho.com>
2022-09-21 01:43:08 -0500

    Adding game framework unit tests
    Some infrastructure for testing pipeline content loading.

---
+++ tests/Game.Tests/Content/Tiles/Grasslands.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="Grasslands" tilewidth="16" tileheight="16" tilecount="10" columns="5">
+ <image source="Grasslands.png" width="80" height="32"/>
+</tileset>
---
+++ tests/Game.Tests/TileMapTests.cs
@@ -0,0 +1,43 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2022 Bad Echo LLC. All rights reserved.
+//
+//		Bad Echo Technologies are licensed under a
+//		GNU Affero General Public License v3.0.
+//
+//		See accompanying file LICENSE.md or a copy at:
+//		https://www.gnu.org/licenses/agpl-3.0.html
+// </copyright>
+//-----------------------------------------------------------------------
+
+using BadEcho.Game.Tiles;
+using Xunit;
+
+namespace BadEcho.Game.Tests;
+
+public class TileMapTests
+{
+    public TileMapTests()
+    {
+    }
+
+    [Fact]
+    public void Load_GrassFourTiles_NotNull()
+        => ContentTestRunner.Run(g =>
+                                 {
+                                     TileMap map = g.Content.Load<TileMap>("Tiles\\GrassFourTiles");
+
+                                     Assert.NotNull(map);
+                                 });
+    [Fact]
+    public void Load_GrassFourTiles_HasTileLayer()
+        => ContentTestRunner.Run(g =>
+                                 {
+                                     TileMap map = g.Content.Load<TileMap>("Tiles\\GrassFourTiles");
+
+                                     var tileLayer = map.Layers.OfType<TileLayer>().FirstOrDefault();
+
+                                     Assert.NotNull(tileLayer);
+                                 });
+}
