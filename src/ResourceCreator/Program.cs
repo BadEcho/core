46c42576ec9bc193cadd10d5819a1f5bc72b4cfe
Matt Weber <matt@badecho.com>
2022-10-03 19:01:01 -0500

    Added resource-creator dotnet tool

---
+++ src/ResourceCreator/Program.cs
@@ -0,0 +1,71 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2022 Bad Echo LLC. All rights reserved.
+//
+//		Bad Echo Technologies are licensed under a
+//		GNU Affero General Public License v3.0.
+//
+//		See accompanying file LICENSE.md or a copy at:
+//		https://www.gnu.org/licenses/agpl-3.0.html
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Diagnostics;
+using System.Resources;
+using BadEcho.Extensions;
+using BadEcho.ResourceCreator.Properties;
+
+namespace BadEcho.ResourceCreator;
+
+internal class Program
+{
+    /// <summary>
+    /// Entry point for the resource creator application.
+    /// </summary>
+    /// <param name="args">An array of command line arguments passed to the application.</param>
+    public static void Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine(Strings.Usage.CulturedFormat(Process.GetCurrentProcess().ProcessName));
+            return;
+        }
+
+        var resourcesDirectory = args[0];
+
+        if (!Directory.Exists(resourcesDirectory))
+        {
+            Console.WriteLine(Strings.ResourcesDirectoryNotFound);
+            return;
+        }
+
+        var outputFile = args[1];
+
+        CreateResources(resourcesDirectory, outputFile);
+
+        Console.WriteLine(Strings.CreatedResources.CulturedFormat(outputFile));
+    }
+
+    private static void CreateResources(string resourcesDirectory, string outputFile)
+    {
+        using (var resourceWriter = new ResourceWriter(Path.Combine(resourcesDirectory, outputFile)))
+        {
+            foreach (string resourceFile in Directory.GetFiles(resourcesDirectory))
+            {
+                if (Path.GetFileName(resourceFile).Equals(outputFile, StringComparison.OrdinalIgnoreCase))
+                    continue;
+
+                // We typically should initialize streams within a using block, however we need to write the asset as a stream,
+                // and the streams aren't actually read from until we actually generate the resource file.
+                // Because this is a short running process, however, there is no harm and no foul here.
+                var stream = new FileStream(resourceFile, FileMode.Open);
+                string resourceName = Path.GetFileNameWithoutExtension(resourceFile);
+
+                resourceWriter.AddResource(resourceName, stream);
+            }
+
+            resourceWriter.Generate();
+        }
+    }
+}
---
+++ src/ResourceCreator/README.md
@@ -0,0 +1,20 @@
+﻿# Bad Echo Resource Creator
+[![Discord](https://img.shields.io/discord/348353194801364992?style=flat-square&label=Discord&logo=discord&logoColor=white&color=7289DA)](https://discord.gg/omni) 
+
+The `BadEcho.ResourceCreator` tool is used to generate **.resources** files that contain files that we wish to bundle as resources for a program. The resulting **.resources** files created by this tool can be compiled as embedded resources and then loaded through the use of a `ResourceManager`.
+
+Visual Studio's built-in code generator for **.resx** files fails to function correctly when it comes to embedding audio resources. Manual creation of a **.resources** file with this tool allows the loading of audio assets by a program to be both possible and fairly simple.
+
+This is a dotnet tool, with the command name `resource-creator`.
+
+This package exists to be used by specific Bad Echo applications, but is licensed under the GNU Affero General Public License so that others may enjoy it as well; see the accompanying [license](https://github.com/omniscientist/BadEcho/blob/master/LICENSE.md) for details.
+
+## About Bad Echo
+Bad Echo is a collection of software technologies and [various writings](https://badecho.com) by Matt Weber: a software designer, partnered [Twitch](https://twitch.tv/omni) streamer, and game developer.
+
+While Bad Echo code concerns itself a great deal with the "best approaches" to general software problems, its ultimate focus and goal is to provide entertainment through the clever manipulation of games and the creation of new ones.
+
+## Getting in Contact
+I'm a partnered Twitch streamer, and talking to me during one of my [Omnified streams](https://twitch.tv/omni), on the off chance that I actually _am_ streaming, is most definitely the quickest way to get my attention!
+
+You may also reach me [via email](mailto:matt@badecho.com).
