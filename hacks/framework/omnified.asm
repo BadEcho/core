db5320907e219ca6b18a5a42877c7f93b7af1320
Matt Weber <matt@badecho.com>
2022-10-23 00:42:19 -0500

    R9 needs to be preserved (msvcrt.rand)

--- hacks/framework/omnified.asm
+++ hacks/framework/omnified.asm
@@ -141,6 +141,7 @@ generateRandomNumber:
     push rcx
     push rdx
     push r8
+    push r9
     push r10
     push r11
     // Load the current thread ID from the Thread Information Block.
@@ -182,7 +183,8 @@ getRandomNumber:
     mov eax,edx
     pop r11
     pop r10
-    pop r8
+    pop r9
+    pop r8    
     pop rdx
     pop rcx
     pop rbx
