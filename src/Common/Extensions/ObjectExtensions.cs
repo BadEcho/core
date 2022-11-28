19c2a3fb7fe8c29be2768743af46cbf53f623f4c
Matt Weber <matt@badecho.com>
2022-11-28 05:19:01 -0600

    Four parameter GetHashCode

--- src/Common/Extensions/ObjectExtensions.cs
+++ src/Common/Extensions/ObjectExtensions.cs
@@ -100,4 +100,25 @@ public static class ObjectExtensions
     /// </remarks>
     public static int GetHashCode<T, T1, T2, T3>(this T source, T1 firstProperty, T2 secondProperty, T3 thirdProperty)
         => HashCode.Combine(firstProperty, secondProperty, thirdProperty);
+
+    /// <summary>
+    /// Calculates the hash code for this instance using three of the object's properties.
+    /// </summary>
+    /// <typeparam name="T">The type of the object the hash code is being calculated for.</typeparam>
+    /// <typeparam name="T1">The type of the first property the hash code is based on.</typeparam>
+    /// <typeparam name="T2">The type of the second property the hash code is based on.</typeparam>
+    /// <typeparam name="T3">The type of the third property the hash code is based on.</typeparam>
+    /// <typeparam name="T4">The type of the fourth property the hash code is based on.</typeparam>
+    /// <param name="source">The object we're calculating the hash code for.</param>
+    /// <param name="firstProperty">The first property to use in calculating the hash code.</param>
+    /// <param name="secondProperty">The second property to use in calculating the hash code.</param>
+    /// <param name="thirdProperty">The third property to use in calculating the hash code.</param>
+    /// <param name="fourthProperty">The fourth property to use in calculating the hash code.</param>
+    /// <returns>A has code for <c>source</c> based on the provided property values.</returns>
+    /// <remarks>
+    /// Generic type parameters are present for <c>source</c> and all other properties in order to avoid the potential
+    /// boxing of any values types.
+    /// </remarks>
+    public static int GetHashCode<T, T1, T2, T3, T4>(this T source, T1 firstProperty, T2 secondProperty, T3 thirdProperty, T4 fourthProperty)
+        => HashCode.Combine(firstProperty, secondProperty, thirdProperty, fourthProperty);
 }
