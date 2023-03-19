d4bcb0a92160b0bc67737a0a316a10d4a8a9895e
Matt Weber <matt@badecho.com>
2023-03-19 05:01:12 -0500

    Resolving warnings for sake of repo-reorg release

--- src/Game/Collisions/CollisionEngine.cs
+++ src/Game/Collisions/CollisionEngine.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//		Created by Matt Weber <matt@badecho.com>
+//		Copyright @ 2023 Bad Echo LLC. All rights reserved.
+//
+//		Bad Echo Technologies are licensed under a
+//		GNU Affero General Public License v3.0.
+//
+//		See accompanying file LICENSE.md or a copy at:
+//		https://www.gnu.org/licenses/agpl-3.0.html
+// </copyright>
+//-----------------------------------------------------------------------
 
 namespace BadEcho.Game.Collisions;
-internal class CollisionEngine
+
+internal static class CollisionEngine
 {
 }
