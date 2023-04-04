e1ffee8e25934b058086353bf83e90e8c566e3fa
Matt Weber <matt@badecho.com>
2023-04-04 18:11:38 -0500

    Replacing tabs with spaces in copyright header

--- src/Game.Pipeline/BoundedTextureWriter.cs
+++ src/Game.Pipeline/BoundedTextureWriter.cs
@@ -3,7 +3,7 @@
 //      Created by Matt Weber <matt@badecho.com>
 //      Copyright @ 2023 Bad Echo LLC. All rights reserved.
 //
-//      Bad Echo Technologies are licensed under a
+//      Bad Echo Technologies are licensed under the
 //      GNU Affero General Public License v3.0.
 //
 //      See accompanying file LICENSE.md or a copy at:
--- src/Presentation/Behaviors/CommandAction.cs
+++ src/Presentation/Behaviors/CommandAction.cs
@@ -1,13 +1,13 @@
 ﻿//-----------------------------------------------------------------------
 // <copyright>
-//		Created by Matt Weber <matt@badecho.com>
-//		Copyright @ 2023 Bad Echo LLC. All rights reserved.
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2023 Bad Echo LLC. All rights reserved.
 //
-//		Bad Echo Technologies are licensed under the
-//		GNU Affero General Public License v3.0.
+//      Bad Echo Technologies are licensed under the
+//      GNU Affero General Public License v3.0.
 //
-//		See accompanying file LICENSE.md or a copy at:
-//		https://www.gnu.org/licenses/agpl-3.0.html
+//      See accompanying file LICENSE.md or a copy at:
+//      https://www.gnu.org/licenses/agpl-3.0.html
 // </copyright>
 //-----------------------------------------------------------------------
 
