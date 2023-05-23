206ffa6f4b4d494ffdb83d51c13998a33ce2d798
Matt Weber <matt@badecho.com>
2023-05-23 13:59:35 -0500

    Added game state base class

---
+++ src/Game/States/GameState.cs
@@ -0,0 +1,47 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2023 Bad Echo LLC. All rights reserved.
+//
+//      Bad Echo Technologies are licensed under the
+//      GNU Affero General Public License v3.0.
+//
+//      See accompanying file LICENSE.md or a copy at:
+//      https://www.gnu.org/licenses/agpl-3.0.html
+// </copyright>
+//-----------------------------------------------------------------------
+
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadEcho.Game.States;
+
+/// <summary>
+/// Provides a scene for a game with independently managed content.
+/// </summary>
+public abstract class GameState
+{
+    public void Load()
+    {
+
+    }
+
+    public void Unload()
+    {
+
+    }
+
+    public void Update(GameUpdateTime time)
+    {
+
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+
+    }
+}
