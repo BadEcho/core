40713e37de4fd42fc389d92b08c659fdc2ee2dfc
Matt Weber <matt@badecho.com>
2023-07-20 15:06:29 -0500

    Adding attached callback to UserInterface

--- src/Game/UI/UserInterface.cs
+++ src/Game/UI/UserInterface.cs
@@ -10,14 +10,13 @@
 //      https://www.gnu.org/licenses/agpl-3.0.html
 // </copyright>
 //-----------------------------------------------------------------------
-
 using Microsoft.Xna.Framework.Content;
 
 namespace BadEcho.Game.UI;
 
 /// <summary>
 /// Provides a self-contained user interface configuration that loads and deploys packaged controls onto a provided
-/// <see cref="Screen"/> instances.x
+/// <see cref="Screen"/> instances.
 /// </summary>
 public abstract class UserInterface
 {
@@ -25,21 +24,32 @@ public abstract class UserInterface
     /// Attaches this user interface onto the provided screen.
     /// </summary>
     /// <param name="screen">The screen to display this user interface on.</param>
-    public void Attach(Screen screen)
+    /// <param name="contentManager">The content manager to use to load the content's dependencies.</param>
+    public void Attach(Screen screen, ContentManager contentManager)
     {
+        Require.NotNull(screen, nameof(screen));
+        Require.NotNull(contentManager, nameof(contentManager));
 
+        LoadContent(contentManager);
+
+        screen.Content = LoadControls();
     }
 
     /// <summary>
     /// Loads resources that this interface's controls depend on using the provided content manager.
     /// </summary>
-    /// <param name="contentManager">The content manager to use to load the content.</param>
-    protected virtual void LoadContent(ContentManager contentManager)
-    { }
+    /// <param name="contentManager">The content manager to use to load the content's dependencies.</param>
+    protected abstract void LoadContent(ContentManager contentManager);
 
     /// <summary>
     /// Initializes and returns a layout panel containing this interface's controls.
     /// </summary>
     /// <returns>A <see cref="Panel"/> instance containing this interface's controls.</returns>
     protected abstract Panel LoadControls();
+
+    /// <summary>
+    /// Called when this user interface has been attached to a screen.
+    /// </summary>
+    protected virtual void OnAttached()
+    { }
 }
