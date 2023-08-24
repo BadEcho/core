094811e9f87e86f103dbf391d1c522f5da54de86
Matt Weber <matt@badecho.com>
2023-08-24 13:16:55 -0500

    Test file for MSDF assets

--- tests/Game.Tests/DistanceFieldFontTests.cs
+++ tests/Game.Tests/DistanceFieldFontTests.cs
@@ -11,24 +11,8 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+namespace BadEcho.Game.Tests;
 
-namespace BadEcho.Game.Pipeline.DistanceFieldFonts;
-
-/// <summary>
-/// Provides configuration data for a multi-channel signed distance field font.
-/// </summary>
-public sealed class DistanceFieldFontAsset
+public class DistanceFieldFontTests
 {
-    /// <summary>
-	/// Initializes a new instance of the <see cref="DistanceFieldFontAsset"/> class.
-	/// </summary>
-    public DistanceFieldFontAsset()
-    {
-
-    }
 }
