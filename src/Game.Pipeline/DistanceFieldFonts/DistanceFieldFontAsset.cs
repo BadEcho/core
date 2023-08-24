f3079a9251760ebfe0eb8f1552642a737e9ca360
Matt Weber <matt@badecho.com>
2023-08-24 08:07:20 -0500

    Start of MSDF support to pipeline....

---
+++ src/Game.Pipeline/DistanceFieldFonts/DistanceFieldFontAsset.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadEcho.Game.Pipeline.DistanceFieldFonts;
+
+/// <summary>
+/// Provides configuration data for a multi-channel signed distance field font.
+/// </summary>
+public sealed class DistanceFieldFontAsset
+{
+}
