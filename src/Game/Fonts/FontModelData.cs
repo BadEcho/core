04de40d742bbf6db0d23ecc1e570ffbe66333931
Matt Weber <matt@badecho.com>
2023-09-06 23:04:12 -0500

    Added FontModelData for MSDF 3D modeling

---
+++ src/Game/Fonts/FontModelData.cs
@@ -0,0 +1,98 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2023 Bad Echo LLC. All rights reserved.
+//
+//      Bad Echo Technologies are licensed under the
+//      GNU Affero General Public License v3.0.
+//
+//      See accompanying file LICENSE.md or a copy at:
+//      https://www.gnu.org/licenses/agpl-3.0.html
+// </copyright>
+//-----------------------------------------------------------------------
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BadEcho.Game.Fonts;
+
+/// <summary>
+/// Provides the vertex data required to render a 3D model of flat signed distance field font text.
+/// </summary>
+public sealed class FontModelData : QuadModelData<VertexPositionColorTexture>
+{
+    private readonly Color _color;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FontModelData"/> class.
+    /// </summary>
+    /// <param name="color">The color of the text.</param>
+    public FontModelData(Color color)
+    {
+        _color = color;
+    }
+
+    /// <summary>
+    /// Adds 3D modeling data for a quadrilateral surface that can be mapped to a glyph found in a font atlas texture during
+    /// rendering.
+    /// </summary>
+    /// <param name="glyph">The font glyph to model.</param>
+    /// <param name="characteristics">Characteristics of the font the glyph belongs to.</param>
+    /// <param name="cursor">The current position of the text's cursor.</param>
+    /// <param name="scaledAdvance">
+    /// The current advance width with the desired scaling applied to it, which helps determine the position of the text.
+    /// </param>
+    /// <remarks>
+    /// <para>
+    /// In order to model a glyph, we create <see cref="VertexPositionColorTexture"/> values whose texture coordinates are
+    /// based on the generated atlas coordinates for the glyph. These coordinates must be normalized such that they are in a range
+    /// from 0 to 1 where (0, 0) is the top-left of the texture and (1, 1) is the bottom-right of the texture.
+    /// </para>
+    /// <para>
+    /// Much like what we do with <see cref="QuadTextureModelData"/>, we divide the atlas (source) rectangle's individual vertex coordinates by the
+    /// appropriate font atlas texture dimension, based on the axis that the particular vertex rests on.
+    /// </para>
+    /// <para>
+    /// Unlike what we do with <see cref="QuadTextureModelData"/>, the source rectangle has no impact on the position of the vertices; rather, we
+    /// make base the position off of the generated plane coordinates for the glyph. These coordinates are relative to the baseline cursor,
+    /// and allow us to position the glyphs appropriately.
+    /// </para>
+    /// </remarks>
+    public void AddGlyph(FontGlyph glyph, FontCharacteristics characteristics, Vector2 cursor, Vector2 scaledAdvance)
+    {
+        Require.NotNull(glyph, nameof(glyph));
+        Require.NotNull(characteristics, nameof(characteristics));
+
+        RectangleF atlasBounds = glyph.AtlasBounds;
+        float texelLeft = atlasBounds.X / characteristics.Width;
+        float texelRight = (atlasBounds.X + atlasBounds.Width) / characteristics.Width;
+        float texelTop = atlasBounds.Y / characteristics.Height;
+        float texelBottom = (atlasBounds.Y + atlasBounds.Height) / characteristics.Height;
+
+        Vector2 verticalAdvance = -1 * new Vector2(scaledAdvance.Y, -scaledAdvance.X);
+        RectangleF planeBounds = glyph.PlaneBounds;
+        Vector2 positionLeft = scaledAdvance * planeBounds.Left;
+        Vector2 positionRight = scaledAdvance * planeBounds.Right;
+        Vector2 positionTop = verticalAdvance * planeBounds.Top;
+        Vector2 positionBottom = verticalAdvance * planeBounds.Bottom;
+
+        var vertexTopLeft
+            = new VertexPositionColorTexture(new Vector3(cursor + positionTop + positionLeft, 1),
+                                             _color,
+                                             new Vector2(texelLeft, texelTop));
+        var vertexTopRight
+            = new VertexPositionColorTexture(new Vector3(cursor + positionTop + positionRight, 1),
+                                             _color,
+                                             new Vector2(texelRight, texelTop));
+        var vertexBottomLeft
+            = new VertexPositionColorTexture(new Vector3(cursor + positionBottom + positionLeft, 1),
+                                             _color,
+                                             new Vector2(texelLeft, texelBottom));
+        var vertexBottomRight
+            = new VertexPositionColorTexture(new Vector3(cursor + positionBottom + positionRight, 1),
+                                             _color,
+                                             new Vector2(texelRight, texelBottom));
+
+        AddVertices(vertexTopLeft, vertexTopRight, vertexBottomLeft, vertexBottomRight);
+    }
+}
--- tests/Game.Tests/DistanceFieldFontTests.cs
+++ tests/Game.Tests/DistanceFieldFontTests.cs
@@ -14,6 +14,7 @@
 using BadEcho.Game.Fonts;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 using Xunit;
 
 namespace BadEcho.Game.Tests;
@@ -21,9 +22,13 @@ namespace BadEcho.Game.Tests;
 public class DistanceFieldFontTests : IClassFixture<ContentManagerFixture>
 {
     private readonly ContentManager _content;
+    private readonly GraphicsDevice _device;
 
     public DistanceFieldFontTests(ContentManagerFixture contentFixture)
-        => _content = contentFixture.Content;
+    {
+        _content = contentFixture.Content;
+        _device = contentFixture.Device;
+    }
 
     [Fact]
     public void Load_Lato_NotNull()
@@ -38,17 +43,116 @@ public class DistanceFieldFontTests : IClassFixture<ContentManagerFixture>
     }
 
     [Fact]
-    public void GetNextAdvance_Lato_ReturnsValid()
+    public void CreateModelData_Lato_ReturnsValid()
     {
         DistanceFieldFont font = _content.Load<DistanceFieldFont>("Fonts\\Lato");
 
-        Vector2 direction = new(1, 0);
-        float scale = 1.0f;
+        var modelData = font.CreateModelData("Hi", new Vector2(20, 20), Color.White, 32);
+        
+        var vertexBuffer = new VertexBuffer(_device,
+                                            VertexPositionColorTexture.VertexDeclaration,
+                                            8,
+                                            BufferUsage.None);
 
-        Vector2 expectedAdvance = direction * 0.5655f * scale;
+        modelData.LoadVertices(vertexBuffer);
 
-        expectedAdvance += direction * -0.093f * scale;
+        VertexPositionColorTexture[] vertices = new VertexPositionColorTexture[8];
 
-        Assert.Equal(expectedAdvance, font.GetNextAdvance('F', 'J', direction, scale));
+        vertexBuffer.GetData(vertices);
+
+        var expectedVertices = new VertexPositionColorTexture[]
+                               {
+                                   new(new Vector3(21.716f, 21.7960014f, 1f),
+                                       Color.White,
+                                       new Vector2(0.286317557f, 0.8353041f)),
+                                   new(new Vector3(42.716f, 21.7960014f, 1f),
+                                       Color.White,
+                                       new Vector2(0.3572635f, 0.8353041f)),
+                                   new(new Vector3(21.716f, 46.796f, 1f),
+                                       Color.White,
+                                       new Vector2(0.286317557f, 0.9197635f)),
+                                   new(new Vector3(42.716f, 46.796f, 1f),
+                                       Color.White,
+                                       new Vector2(0.3572635f, 0.9197635f)),
+                                   new(new Vector3(45.022f, 21.3380013f, 1f),
+                                       Color.White,
+                                       new Vector2(0.9771959f, 0.0008445946f)),
+                                   new(new Vector3(51.522f, 21.3380013f, 1f),
+                                       Color.White,
+                                       new Vector2(0.9991554f, 0.0008445946f)),
+                                   new(new Vector3(45.022f, 46.838f, 1f),
+                                       Color.White,
+                                       new Vector2(0.9771959f, 0.08699324f)),
+                                   new(new Vector3(51.522f, 46.838f, 1f),
+                                       Color.White,
+                                       new Vector2(0.9991554f, 0.08699324f))
+                               };
+
+        Assert.Collection(vertices,
+                          t => Assert.Equal(expectedVertices[0], t),
+                          t => Assert.Equal(expectedVertices[1], t),
+                          t => Assert.Equal(expectedVertices[2], t),
+                          t => Assert.Equal(expectedVertices[3], t),
+                          t => Assert.Equal(expectedVertices[4], t),
+                          t => Assert.Equal(expectedVertices[5], t),
+                          t => Assert.Equal(expectedVertices[6], t),
+                          t => Assert.Equal(expectedVertices[7], t));
+    }
+
+    [Fact]
+    public void CreateModelData_LatoKerning_ReturnsValid()
+    { 
+        DistanceFieldFont font = _content.Load<DistanceFieldFont>("Fonts\\Lato");
+        // A kerning adjustment exists in Lato for L + v.
+        var modelData = font.CreateModelData("Lv", new Vector2(20, 20), Color.White, 32);
+        
+        var vertexBuffer = new VertexBuffer(_device,
+                                            VertexPositionColorTexture.VertexDeclaration,
+                                            8,
+                                            BufferUsage.None);
+
+        modelData.LoadVertices(vertexBuffer);
+
+        VertexPositionColorTexture[] vertices = new VertexPositionColorTexture[8];
+
+        vertexBuffer.GetData(vertices);
+
+        var expectedVertices = new VertexPositionColorTexture[]
+                               {
+                                   new(new Vector3(21.682f, 21.7960014f, 1f),
+                                       Color.White,
+                                       new Vector2(0.7440878f, 0.8724662f)),
+                                   new(new Vector3(37.182f, 21.7960014f, 1f),
+                                       Color.White,
+                                       new Vector2(0.7964527f, 0.8724662f)),
+                                   new(new Vector3(21.682f, 46.796f, 1f),
+                                       Color.White,
+                                       new Vector2(0.7440878f, 0.9569257f)),
+                                   new(new Vector3(37.182f, 46.796f, 1f),
+                                       Color.White,
+                                       new Vector2(0.7964527f, 0.9569257f)),
+                                   new(new Vector3(32.744f, 28.406002f, 1f),
+                                       Color.White,
+                                       new Vector2(0.504223f, 0.0008445946f)),
+                                   new(new Vector3(50.744f, 28.406002f, 1f),
+                                       Color.White,
+                                       new Vector2(0.5650338f, 0.0008445946f)),
+                                   new(new Vector3(32.744f, 46.906002f, 1f),
+                                       Color.White,
+                                       new Vector2(0.504223f, 0.0633446f)),
+                                   new(new Vector3(50.744f, 46.906002f, 1f),
+                                       Color.White,
+                                       new Vector2(0.5650338f, 0.0633446f))
+                               };
+
+        Assert.Collection(vertices,
+                          t => Assert.Equal(expectedVertices[0], t),
+                          t => Assert.Equal(expectedVertices[1], t),
+                          t => Assert.Equal(expectedVertices[2], t),
+                          t => Assert.Equal(expectedVertices[3], t),
+                          t => Assert.Equal(expectedVertices[4], t),
+                          t => Assert.Equal(expectedVertices[5], t),
+                          t => Assert.Equal(expectedVertices[6], t),
+                          t => Assert.Equal(expectedVertices[7], t));
     }
 }
