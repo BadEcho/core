5a5a0ecbd97457f6c1f85749be20093b957973f0
Matt Weber <matt@badecho.com>
2023-11-03 18:08:17 -0500

    Missing xmldoc. Will fill in functionality soon :)

--- src/Game/States/LoadingState.cs
+++ src/Game/States/LoadingState.cs
@@ -1,8 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2023 Bad Echo LLC. All rights reserved.
+//
+//      Bad Echo Technologies are licensed under the
+//      GNU Affero General Public License v3.0.
+//
+//      See accompanying file LICENSE.md or a copy at:
+//      https://www.gnu.org/licenses/agpl-3.0.html
+// </copyright>
+//-----------------------------------------------------------------------
+
 using Microsoft.Xna.Framework.Content;
 
 namespace BadEcho.Game.States;
@@ -13,11 +21,13 @@ namespace BadEcho.Game.States;
 /// </summary>
 public sealed class LoadingState : GameState
 {
+    /// <inheritdoc/>
     protected override void DrawCore(ConfiguredSpriteBatch spriteBatch)
     {
-        
+
     }
 
+    /// <inheritdoc/>
     protected override void LoadContent(ContentManager contentManager)
     {
         
