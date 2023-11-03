77f2c045d3065468f3a7188a5c1b1d16fc64367b
Matt Weber <matt@badecho.com>
2023-11-03 02:12:35 -0500

    Adding an unfinished loading state

---
+++ src/Game/States/LoadingState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+
+namespace BadEcho.Game.States;
+
+/// <summary>
+/// Provides a game state that acts a loading screen, used to distract the player from the fact that the game takes so
+/// damn long to load.
+/// </summary>
+public sealed class LoadingState : GameState
+{
+    protected override void DrawCore(ConfiguredSpriteBatch spriteBatch)
+    {
+        
+    }
+
+    protected override void LoadContent(ContentManager contentManager)
+    {
+        
+    }
+}
