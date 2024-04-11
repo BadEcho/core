0690c1bdb123bf1798ccb36c899144eae96afd27
Matt Weber <matt@badecho.com>
2024-04-11 16:06:09 -0500

    Only constant speed needed
    Will be getting rid of "movement systems" in upcoming refactor.

--- src/Game/PlayerMovementSystem.cs
+++ src/Game/PlayerMovementSystem.cs
@@ -11,7 +11,6 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
-using BadEcho.Extensions;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 
@@ -28,21 +27,16 @@ public sealed class PlayerMovementSystem : IMovementSystem
     private const Keys MOVEMENT_RIGHT = Keys.D;
     private const Keys MOVEMENT_DOWN = Keys.S;
     // TODO: Will be configurable based on entity.
-    private const float VELOCITY_INCREMENT = .3f;
-    private const float FRICTION = 0.05f;
-    private const float VELOCITY_MAX = 1.5f;
-    private const float VELOCITY_MIN = -1.5f;
+    private const float VELOCITY_MAX = 1.25f;
+    private const float VELOCITY_MIN = -1.25f;
 
     /// <inheritdoc/>
     public void UpdateMovement(IPositionalEntity entity)
     {
         Require.NotNull(entity, nameof(entity));
 
-        float xVelocityOriginal = entity.Velocity.X;
-        float yVelocityOriginal = entity.Velocity.Y;
-
-        float xVelocity = CalculateUpdatedVelocity(xVelocityOriginal, MOVEMENT_RIGHT, MOVEMENT_LEFT);
-        float yVelocity = CalculateUpdatedVelocity(yVelocityOriginal, MOVEMENT_DOWN, MOVEMENT_UP);
+        float xVelocity = CalculateUpdatedVelocity(MOVEMENT_RIGHT, MOVEMENT_LEFT);
+        float yVelocity = CalculateUpdatedVelocity(MOVEMENT_DOWN, MOVEMENT_UP);
 
         entity.Velocity = new Vector2(xVelocity, yVelocity);
     }
@@ -55,38 +49,16 @@ public sealed class PlayerMovementSystem : IMovementSystem
         entity.Position += penetration;
     }
 
-    private static float CalculateUpdatedVelocity(float currentVelocity, Keys positiveDirectionKey, Keys negativeDirectionKey)
+    private static float CalculateUpdatedVelocity(Keys positiveDirectionKey, Keys negativeDirectionKey)
     {
         KeyboardState keyboardState = Keyboard.GetState();
-        double velocityDelta = 0;
 
         bool positiveMovement = keyboardState.IsKeyDown(positiveDirectionKey);
         bool negativeMovement = keyboardState.IsKeyDown(negativeDirectionKey);
 
         if (!positiveMovement && !negativeMovement)
-        {
-            if (currentVelocity.ApproximatelyEquals(0))
                 return 0;
 
-            velocityDelta += (currentVelocity > 0 ? -1 : 1) * FRICTION;
-        }
-
-        if (positiveMovement)
-        {
-            if (currentVelocity < 0)
-                currentVelocity = 0;
-
-            velocityDelta += VELOCITY_INCREMENT - FRICTION;
-        }
-
-        if (negativeMovement)
-        {
-            if (currentVelocity > 0)
-                currentVelocity = 0;
-
-            velocityDelta -= VELOCITY_INCREMENT + FRICTION;
-        }
-
-        return (float) Math.Min(VELOCITY_MAX, Math.Max(VELOCITY_MIN, currentVelocity + velocityDelta));
+        return positiveMovement ? VELOCITY_MAX : VELOCITY_MIN;
     }
 }
