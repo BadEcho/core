c8825d710439a9b06289ab33546f62e3c42b0a3a
Matt Weber <matt@badecho.com>
2024-04-23 17:18:06 -0500

    Undoing 1743d079
    Caused bouncy collisions, probably due to the endpoint-exclusivity
    of rectangles.

--- src/Game/Quadtree.cs
+++ src/Game/Quadtree.cs
@@ -151,7 +151,7 @@ public sealed class Quadtree
             if (!element.Bounds.Intersects(node.Bounds))
                 continue;
 
-            collisions.AddRange(node._elements.Where(e => element.Bounds.Intersects(e.Bounds)));
+            collisions.AddRange(node._elements.Where(e => e.Bounds.Intersects(element.Bounds)));
 
             if (!node.IsLeaf)
             {
