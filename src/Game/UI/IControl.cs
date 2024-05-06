34e6d98d8cfb376787545bb5b4d248d16e771ae6
Matt Weber <matt@badecho.com>
2024-05-06 13:09:08 -0500

    Merged IControlProperties with IControl

--- src/Game/UI/IControl.cs
+++ src/Game/UI/IControl.cs
@@ -18,8 +18,147 @@ namespace BadEcho.Game.UI;
 /// <summary>
 /// Defines a user interface element in a game.
 /// </summary>
-public interface IControl : IControlProperties, IArrangeable, IInputElement
+public interface IControl : IArrangeable, IInputElement
 {
+    /// <summary>
+    /// Gets the parent of this control.
+    /// </summary>
+    IArrangeable? Parent { get; }
+
+    /// <summary>
+    /// Gets the desired size of this control.
+    /// </summary>
+    Size DesiredSize { get; }
+
+    /// <summary>
+    /// Gets the bounding rectangle of the region occupied by this control, which includes its margin, border, background,
+    /// padding, and content.
+    /// </summary>
+    Rectangle LayoutBounds { get; }
+
+    /// <summary>
+    /// Gets the bounding rectangle of the region occupied by this control's border, which includes its background, padding,
+    /// and content.
+    /// </summary>
+    Rectangle BorderBounds { get; }
+
+    /// <summary>
+    /// Gets the bounding rectangle of the region occupied by this control's background, padding, and content.
+    /// </summary>
+    Rectangle BackgroundBounds { get; }
+
+    /// <summary>
+    /// Gets the bounding rectangle of the region occupied by the control's padding and actual content.
+    /// </summary>
+    Rectangle ContentBounds { get; }
+
+    /// <summary>
+    /// Gets or sets a value indicating if this control is enabled in the user interface and receiving input.
+    /// </summary>
+    bool IsEnabled { get; }
+
+    /// <summary>
+    /// Gets or sets a value indicating if this control is visible.
+    /// </summary>
+    bool IsVisible { get; set; }
+
+    /// <summary>
+    /// Gets or sets the space between this control and other elements adjacent to it when the layout is generated.
+    /// </summary>
+    Thickness Margin { get; set; }
+
+    /// <summary>
+    /// Gets or sets the space between this control's border and its content.
+    /// </summary>
+    Thickness Padding { get; set; }
+
+    /// <summary>
+    /// Gets or sets the thickness of this control's border.
+    /// </summary>
+    Thickness BorderThickness { get; set; }
+
+    /// <summary>
+    /// Gets or sets the specific width of this control.
+    /// </summary>
+    int? Width { get; set; }
+
+    /// <summary>
+    /// Gets or sets the minimum width of this control.
+    /// </summary>
+    int? MinimumWidth { get; set; }
+
+    /// <summary>
+    /// Gets or sets the maximum width of this control.
+    /// </summary>
+    int? MaximumWidth { get; set; }
+
+    /// <summary>
+    /// Gets or sets the specific height of this control.
+    /// </summary>
+    int? Height { get; set; }
+
+    /// <summary>
+    /// Gets or sets the minimum height of this control.
+    /// </summary>
+    int? MinimumHeight { get; set; }
+
+    /// <summary>
+    /// Gets or sets the maximum height of this control.
+    /// </summary>
+    int? MaximumHeight { get; set; }
+
+    /// <summary>
+    /// Gets or sets the index of the column within a parent <see cref="Grid"/> panel containing this control.
+    /// </summary>
+    int Column { get; set; }
+
+    /// <summary>
+    /// Gets or sets the index of the row within a parent <see cref="Grid"/> panel containing this control.
+    /// </summary>
+    int Row { get; set; }
+
+    /// <summary>
+    /// Gets or sets the horizontal alignment characteristics that are applied to this control when composed
+    /// in a layout parent, such as a type of <see cref="Panel"/>.
+    /// </summary>
+    HorizontalAlignment HorizontalAlignment { get; set; }
+
+    /// <summary>
+    /// Gets or sets the vertical alignment characteristics that are applied to this control when composed in
+    /// a layout parent, such as a type of <see cref="Panel"/>.
+    /// </summary>
+    VerticalAlignment VerticalAlignment { get; set; }
+
+    /// <summary>
+    /// Gets or sets the background visual of this control.
+    /// </summary>
+    IVisual? Background { get; set; }
+
+    /// <summary>
+    /// Gets or sets the background visual of this control when it has been disabled via the <see cref="IsEnabled"/> property.
+    /// </summary>
+    IVisual? DisabledBackground { get; set; }
+
+    /// <summary>
+    /// Gets or sets the background visual of this control when the cursor is located over it.
+    /// </summary>
+    IVisual? HoveredBackground { get; set; }
+
+    /// <summary>
+    /// Gets or sets the visual of this control's border.
+    /// </summary>
+    IVisual? Border { get; set; }
+
+    /// <summary>
+    /// Gets or sets the visual of this control's border when it has been disabled via the <see cref="IsEnabled"/> property.
+    /// </summary>
+    IVisual? DisabledBorder { get; set; }
+
+    /// <summary>
+    /// Gets or sets the visual of this control's border when the cursor is located over it.
+    /// </summary>
+    IVisual? HoveredBorder { get; set; }
+
     /// <summary>
     /// Updates the desired size of this control, called during the <c>Measure</c> pass of a layout update.
     /// </summary>
