21429b12ff298affdfb33283725c8928bd47c693
Matt Weber <matt@badecho.com>
2024-07-27 20:41:25 -0500

    Latest .NET broke my IEnumerable expression hack

--- src/Game/UI/Style.cs
+++ src/Game/UI/Style.cs
@@ -11,11 +11,8 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
-using System.Collections;
 using System.Linq.Expressions;
-using System.Reflection;
-using BadEcho.Extensions;
-using BadEcho.Game.Properties;
+using System.Runtime.CompilerServices;
 
 namespace BadEcho.Game.UI;
 
@@ -55,65 +52,32 @@ namespace BadEcho.Game.UI;
 /// Being able to apply styles declaratively like above simplifies their use and keeps our code nice and tidy.
 /// </para>
 /// </example>
-public sealed class Style<T> : IEnumerable
+[CollectionBuilder(typeof(StyleBuilder), nameof(StyleBuilder.Create))]
+public sealed class Style<T>
     where T : IControl
 {
-    private readonly List<Action<T>> _setters = [];
+    private readonly List<(Expression<Func<T, object?>>, object)> _setterExpressions;
+    private readonly List<Action<T>> _setters;
 
-    IEnumerator IEnumerable.GetEnumerator()
-        => _setters.GetEnumerator();
+    internal Style(List<(Expression<Func<T, object?>>, object)> setterExpressions, 
+                   List<Action<T>> setters)
+    {
+        _setterExpressions = setterExpressions;
+        _setters = setters;
+    }
 
     /// <summary>
-    /// Adds a setter that applies a property value to this style.
+    /// Returns an enumerator that iterates through this style's property setter expressions.
     /// </summary>
-    /// <param name="setter">
-    /// An expression accessing the property being styled coupled with the value to apply to said property.
-    /// </param>
+    /// <returns>An enumerator for this style.</returns>
     /// <remarks>
-    /// <para>
-    /// This allows collection initializers and expressions to be used to create an instance of this class,
-    /// in addition to providing a means to add further setters post-initialization. It will convert the
-    /// expression into an <see cref="Action{TControl}"/> that will assign the configured value to the
-    /// property.
-    /// </para>
+    /// This exists merely to satisfy the compiler when initializing instances of this type via collection expressions.
+    /// Iteration of a style is not an expected use case and implementation of a collection-related interface is not required
+    /// for collection expression support.
     /// </remarks>
-    public void Add((Expression<Func<T, object?>> Property, object Value) setter)
-    {
-        Require.NotNull(setter, nameof(setter));
-        
-        ParameterExpression control = Expression.Parameter(typeof(T), nameof(control));
-        Expression propertyAccess = setter.Property.Body;
-        
-        // Expressions accessing properties that return value types will have their member access information
-        // stuffed inside a conversion UnaryExpression (on account of the necessary boxing).
-        if (propertyAccess.NodeType == ExpressionType.Convert) 
-            propertyAccess = ((UnaryExpression) propertyAccess).Operand;
-
-        // If we don't have a member access expression at this point, then it is an invalid expression for a style.
-        if (propertyAccess.NodeType != ExpressionType.MemberAccess)
-            throw new ArgumentException(Strings.StyleSetterNotMemberAccess);
-
-        MemberInfo member = ((MemberExpression) propertyAccess).Member;
-
-        // Only properties are meant to be set by the style.
-        if (member.MemberType != MemberTypes.Property)
-            throw new ArgumentException(Strings.StyleSetterNonPropertyAccess);
-
-        // Only the target control type is meant to be styled.
-        if (member.DeclaringType != null && !typeof(T).IsA(member.DeclaringType))
-            throw new ArgumentException(Strings.StyleSetterNonLocalProperty);
-
-        PropertyInfo property = (PropertyInfo) member;
-        propertyAccess = Expression.Property(control, property);
-
-        Expression propertySetter 
-            = Expression.Assign(propertyAccess, Expression.Constant(setter.Value, property.PropertyType));
-
-        Expression<Action<T>> delegateExpression
-            = Expression.Lambda<Action<T>>(propertySetter, control);
-
-        _setters.Add(delegateExpression.Compile());
-    }
+    [MustDisposeResource]
+    public IEnumerator<(Expression<Func<T, object?>>, object)> GetEnumerator()
+        => _setterExpressions.GetEnumerator();
 
     /// <summary>
     /// Applies this style to the provided control.
