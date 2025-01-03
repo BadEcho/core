0582371c3f6075258c147ebdc11a868091a0987a
Matt Weber <matt@badecho.com>
2025-01-03 14:59:46 -0600

    Breaking up the monorepo

--- tests/ExtensibilityPoint.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Runtime.InteropServices;
-using BadEcho.Extensibility;
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("97fbfcad-90fc-4b5d-8af4-36ee0e37916d")]
-
-[assembly: ExtensibilityPoint]
