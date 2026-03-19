489a3d28a0c721956a60b2286a3f15b17a78a488
Matt Weber <matt@BadEcho.com>
2026-03-18 20:26:31 -0500

    Commit to test version distance grepping

--- src/Extensions/ConfigureWritableOptions.cs
+++ src/Extensions/ConfigureWritableOptions.cs
@@ -4,7 +4,7 @@
 //      Copyright @ 2025 Bad Echo LLC. All rights reserved.
 //
 //      Bad Echo Technologies are licensed under the
-//      GNU Affero General Public License v3.0. 
+//      GNU Affero General Public License v3.0.
 //
 //      See accompanying file LICENSE.md or a copy at:
 //      https://www.gnu.org/licenses/agpl-3.0.html
