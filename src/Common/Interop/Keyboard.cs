661669ad90f102e22ff037a250fa021f770046e5
Matt Weber <matt@BadEcho.com>
2026-05-19 17:38:10 -0500

    Add keyboard interop class

---
+++ src/Common/Interop/Keyboard.cs
@@ -0,0 +1,32 @@
+﻿// -----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2026 Bad Echo LLC. All rights reserved.
+//
+//      Bad Echo Technologies are licensed under the
+//      GNU Affero General Public License v3.0.
+//
+//      See accompanying file LICENSE.md or a copy at:
+//      https://www.gnu.org/licenses/agpl-3.0.html
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace BadEcho.Interop;
+
+/// <summary>
+/// Provides information regarding a keyboard device.
+/// </summary>
+public static class Keyboard
+{
+    /// <summary>
+    /// Determines if the specified key is currently pressed.
+    /// </summary>
+    /// <param name="key">The specified key.</param>
+    /// <returns>True if <c>key</c> is pressed; otherwise, false.</returns>
+    public static bool IsPressed(VirtualKey key)
+    {
+        short state = User32.GetKeyState(key);
+
+        return (state & 0x8000) != 0;
+    }
+}
